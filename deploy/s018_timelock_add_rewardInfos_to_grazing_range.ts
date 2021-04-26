162b9e4c691238a20abe1a9c3b7971dad9f79ab7
abraham-alpaca <abraham.alpaca@gmail.com>
2021-04-26 16:43:58 +0700

    feat: add queue timelock for adding a reward info on a grazing range contract

---
+++ deploy/s018_timelock_add_rewardInfos_to_grazing_range.ts
@@ -0,0 +1,68 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+import { Timelock__factory } from '../typechain'
+
+interface IAddGrazingRangeRewardInfoParam {
+    PHASE_NAME: string
+    CAMPAIGN_ID: string
+    ENDBLOCK: string
+    REWARD_PER_BLOCK: string
+}
+
+type IAddGrazingRangeRewardInfoParamList = Array<IAddGrazingRangeRewardInfoParam>
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+  
+  
+  const TIMELOCK = '0x2D5408f2287BF9F9B05404794459a846651D0a59';
+  const GRAZING_RANGE = ''
+  const EXACT_ETA = '1619265600';
+  const REWARDINFO: IAddGrazingRangeRewardInfoParamList = []
+  
+
+
+
+
+
+
+
+
+
+
+
+  const timelock = Timelock__factory.connect(TIMELOCK, (await ethers.getSigners())[0]);
+
+  for(let i = 0; i < REWARDINFO.length; i++) {
+    const rewardInfo = REWARDINFO[i]
+    console.log(`>> Timelock: Adding a grazing range's reward info: "${rewardInfo.PHASE_NAME}" via Timelock`);
+    await timelock.queueTransaction(
+        GRAZING_RANGE, '0',
+        'addRewardInfo(uint256,uint256,uint256)',
+        ethers.utils.defaultAbiCoder.encode(
+            ['uint256', 'uint256', 'uint256'],
+            [
+                rewardInfo.CAMPAIGN_ID,
+                rewardInfo.ENDBLOCK,
+                rewardInfo.REWARD_PER_BLOCK,
+            ]
+        ), EXACT_ETA
+    );
+    console.log("generate timelock.executeTransaction:")
+    console.log(`await timelock.executeTransaction('${GRAZING_RANGE}', '0', 'addRewardInfo(uint256,uint256,uint256)', ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256', 'uint256'],['${rewardInfo.CAMPAIGN_ID}','${rewardInfo.ENDBLOCK}','${rewardInfo.REWARD_PER_BLOCK}']), ${EXACT_ETA})`)
+    console.log("✅ Done");
+  }
+};
+
+export default func;
+func.tags = ['TimelockAddGrazingRangeRewardInfos'];
