c0e640a1db75793b5db0435210a2434dd7ab3820
spicysquid168 <spicysquid168@gmail.com>
2021-04-29 20:41:23 +0700

    deploy: add new campaign for testing

--- deploy/s018_timelock_add_rewardInfos_to_grazing_range.ts
+++ deploy/s018_timelock_add_rewardInfos_to_grazing_range.ts
@@ -24,11 +24,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   */
   const TIMELOCK = '0xb3c3aE82358DF7fC0bd98629D5ed91767e45c337';
   const GRAZING_RANGE = '0x9Ff38741EB7594aCE7DD8bb8107Da38aEE7005D6'
-  const EXACT_ETA = '1619701920';
+  const EXACT_ETA = '1619703660';
   const REWARDINFO: IAddGrazingRangeRewardInfoParamList = [{
     PHASE_NAME: 'PHASE_1',
-    CAMPAIGN_ID: '1',
-    ENDBLOCK: '6989000',
+    CAMPAIGN_ID: '2',
+    ENDBLOCK: '8405700',
     REWARD_PER_BLOCK: ethers.utils.parseEther('1000').toString()
   }]
   
