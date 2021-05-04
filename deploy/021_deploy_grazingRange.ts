89a3602e524e5f14e95a988b85744bcfe93dc03b
abraham-alpaca <abraham.alpaca@gmail.com>
2021-05-04 23:35:31 +0700

    refactor: add a reward holder address string as an initialization argument

--- deploy/021_deploy_grazingRange.ts
+++ deploy/021_deploy_grazingRange.ts
@@ -17,7 +17,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-
+  const REWARD_HOLDER = ''
 
 
 
@@ -28,7 +28,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     (await ethers.getSigners())[0]
   )) as GrazingRange__factory;
   const grazingRange = await upgrades.deployProxy(
-    GrazingRange
+    GrazingRange,
+    [REWARD_HOLDER]
   ) as GrazingRange;
   await grazingRange.deployed();
   console.log(`>> Deployed at ${grazingRange.address}`);
