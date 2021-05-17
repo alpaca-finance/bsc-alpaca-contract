201c7b50a020c70ad24910a40690d7fb3012aae0
spicysquid168 <spicysquid168@gmail.com>
2021-05-17 20:38:54 +0700

    deploy: double-sided pools set 1 to mainnet

--- deploy/s017_timelock_add_campaigns_to_grazing_range.ts
+++ deploy/s017_timelock_add_campaigns_to_grazing_range.ts
@@ -24,12 +24,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   */
   const TIMELOCK = '0x2D5408f2287BF9F9B05404794459a846651D0a59';
   const GRAZING_RANGE = '0x6bf5b334409cC3FD336Da9A2D3e3F9c870fEb343'
-  const EXACT_ETA = '1621220700';
+  const EXACT_ETA = '1621344600';
   const CAMPAIGNS: IAddGrazingRangeCampaignParamList = [{
-    NAME: 'ibALPACA-pCWS',
+    NAME: 'ibALPACA-SWINGBY',
     STAKING_TOKEN: '0xf1bE8ecC990cBcb90e166b71E368299f0116d421',
-    REWARD_TOKEN: '0xbcf39f0edda668c58371e519af37ca705f2bfcbd',
-    START_BLOCK: '7485000'
+    REWARD_TOKEN: '0x71de20e0c4616e7fcbfdd3f875d568492cbe4739',
+    START_BLOCK: '7542000'
   }]
     
 
