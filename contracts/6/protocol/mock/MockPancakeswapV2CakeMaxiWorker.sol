d7d164521aebc9b8e1830f84481ed8c898e1e9d1
abraham-alpaca <abraham.alpaca@gmail.com>
2021-05-18 23:56:42 +0700

    feat: make a mocked cake maxi worker inherited an IWorker interface

--- contracts/6/protocol/mock/MockPancakeswapV2CakeMaxiWorker.sol
+++ contracts/6/protocol/mock/MockPancakeswapV2CakeMaxiWorker.sol
@@ -9,11 +9,11 @@ import "../../utils/AlpacaMath.sol";
 import "../../utils/SafeToken.sol";
 
 /// @notice Simplified version of worker for testing purpose.
-contract MockPancakeswapV2CakeMaxiWorker {
+contract MockPancakeswapV2CakeMaxiWorker is IWorker {
   using SafeToken for address;
 
-  address public baseToken;
-  address public farmingToken;
+  address public override baseToken;
+  address public override farmingToken;
 
   constructor(address _baseToken, address _farmingToken) public {
     baseToken = _baseToken;
@@ -25,7 +25,7 @@ contract MockPancakeswapV2CakeMaxiWorker {
   /// @param debt The amount of user debt to help the strategy make decisions.
   /// @param data The encoded data, consisting of strategy address and calldata.
   function work(uint256 /* id */, address user, uint256 debt, bytes calldata data)
-    external
+    external override
   {
     (address strat, bytes memory ext) = abi.decode(data, (address, bytes));
     baseToken.safeTransfer(strat, baseToken.myBalance());
@@ -33,4 +33,22 @@ contract MockPancakeswapV2CakeMaxiWorker {
     IStrategy(strat).execute(user, debt, ext);
     baseToken.safeTransfer(msg.sender, baseToken.myBalance());
   }
+
+  /// @dev Re-invest whatever the worker is working on.
+  function reinvest() external override {}
+
+  /// @dev Return the amount of wei to get back if we are to liquidate the position.
+  function health(uint256 id) external override view returns (uint256) { return 0; }
+
+  /// @dev Liquidate the given position to token. Send all token back to its Vault.
+  function liquidate(uint256 id) external override {}
+
+  /// @dev SetStretegy that be able to executed by the worker.
+  function setStrategyOk(address[] calldata strats, bool isOk) external override {}
+
+  /// @dev Set address that can be reinvest
+  function setReinvestorOk(address[] calldata reinvestor, bool isOk) external override {}
+
+  /// @dev LP token holds by worker
+  function lpToken() external override view returns (IPancakePair) { address(0); }
 }
