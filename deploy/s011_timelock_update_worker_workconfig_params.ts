6216d1f7e4d7f31f8d7ba75f4933a72dba5edf42
spicysquid168 <spicysquid168@gmail.com>
2021-06-01 17:27:38 +0700

    chore: adjust json file to match with new structure

--- deploy/s011_timelock_update_worker_workconfig_params.ts
+++ deploy/s011_timelock_update_worker_workconfig_params.ts
@@ -14,18 +14,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const WORKER_CONFIG_ADDR = '0x8ae5e14864090E9332Ceb238F7cEa183d7C056a7';
+  const WORKER_CONFIG_ADDR = '0xADaBC5FC5da42c85A84e66096460C769a151A8F8';
 
   const UPDATES = [{
-    WORKER_ADDRESS: '0xA950ee51Ac3b27a1a6C87D6448D6717ACBc7b0A8',
+    WORKER_ADDRESS: '0xC5954CA8988988362f60498d5aDEc67BA466492B',
     ACCEPT_DEBT: true,
-    WORK_FACTOR: '7000',
-    KILL_FACTOR: '8333',
-    MAX_PRICE_DIFF: '11000000',
+    WORK_FACTOR: '8600',
+    KILL_FACTOR: '9200',
+    MAX_PRICE_DIFF: '11000',
   }]
 
-  const TIMELOCK = '0xb3c3aE82358DF7fC0bd98629D5ed91767e45c337';
-  const EXACT_ETA = '1620900720';
+  const TIMELOCK = '0x2D5408f2287BF9F9B05404794459a846651D0a59';
+  const EXACT_ETA = '1622300700';
 
 
 
