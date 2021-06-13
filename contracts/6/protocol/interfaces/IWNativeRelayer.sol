d77e92d7d9a31e3167bbdb7d71be0ad5354a0390
spicysquid168 <spicysquid168@gmail.com>
2021-06-13 12:40:01 +0700

    chore: update license

--- contracts/6/protocol/interfaces/IWNativeRelayer.sol
+++ contracts/6/protocol/interfaces/IWNativeRelayer.sol
@@ -1,3 +1,16 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
 pragma solidity 0.6.6;
 
 interface IWNativeRelayer {
