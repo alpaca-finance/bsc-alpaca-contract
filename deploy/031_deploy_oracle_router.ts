214840896d4d6f311b6883cd5cbaf799792a4f22
jakeal-papaca <jakeal.papaca@gmail.com>
2021-06-15 12:16:07 +0700

    feat: create deploy script

---
+++ deploy/031_deploy_oracle_router.ts
@@ -0,0 +1,44 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers, upgrades } from 'hardhat';
+import { OracleRouter__factory } from '../typechain';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log(">> Deploying an upgradable OracleRouter contract");
+  const OracleRouter = (await ethers.getContractFactory(
+    'OracleRouter',
+    (await ethers.getSigners())[0]
+  )) as OracleRouter__factory;
+  const oracleRouter = await upgrades.deployProxy(
+    OracleRouter
+  );
+  await oracleRouter._deployed();
+  console.log(`>> Deployed at ${oracleRouter.address}`);
+};
+
+export default func;
+func.tags = ['OracleRouter'];
