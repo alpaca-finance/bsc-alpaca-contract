852112c3dbd2e52a0c22a6d8e477850b9e3e38db
jakeal-papaca <jakeal.papaca@gmail.com>
2021-06-17 01:13:20 +0700

    feat: create chain link price oracle contract

---
+++ contracts/6/protocol/mock/MockAggregatorV3.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract MockAggregatorV3 {
+  int256 public price;
+  uint8 public decimals;
+  string public description;
+  uint256 public version;
+
+  constructor(int256 _price, uint8 _decimals) public {
+    price = _price;
+    decimals = _decimals;
+  }
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    )
+  {
+    return (uint80(0), price, uint256(0), uint256(0), uint80(0));
+  }
+}
