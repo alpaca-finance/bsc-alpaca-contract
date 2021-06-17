c01ff1648b303acb91d667c516942747b7ad76d1
jakeal-papaca <jakeal.papaca@gmail.com>
2021-06-17 18:13:07 +0700

    test: add test case existed pair

--- test/ChainLinkPriceOracle.test.ts
+++ test/ChainLinkPriceOracle.test.ts
@@ -22,7 +22,8 @@ let alice: Signer
 let token0: MockERC20
 let token1: MockERC20
 
-let mockAggregatorV3: MockAggregatorV3
+let mockAggregatorV3T0T1: MockAggregatorV3
+let mockAggregatorV3T1T0: MockAggregatorV3
 
 let chainLinkOracle: ChainLinkPriceOracle
 let chainLinkOracleAsDeployer: ChainLinkPriceOracle
@@ -42,8 +43,10 @@ describe('ChainLinkPriceOracle', () => {
       'MockAggregatorV3',
       deployer,
     )) as MockAggregatorV3__factory
-    mockAggregatorV3 = await MockAggregatorV3.deploy(BigNumber.from('36538981280'), 8)
-    await mockAggregatorV3.deployed()
+    mockAggregatorV3T0T1 = await MockAggregatorV3.deploy(BigNumber.from('36500000000'), 8)
+    await mockAggregatorV3T0T1.deployed()
+    mockAggregatorV3T1T0 = await MockAggregatorV3.deploy(BigNumber.from('273972'), 8)
+    await mockAggregatorV3T1T0.deployed()
 
     const ChainLinkPriceOracle = (await ethers.getContractFactory(
       'ChainLinkPriceOracle',
@@ -59,18 +62,26 @@ describe('ChainLinkPriceOracle', () => {
     context('when the caller is not the owner', async () => {
       it('should be reverted', async () => {
         await expect(
-          chainLinkOracleAsAlice.setPriceFeed(token0.address, token1.address, mockAggregatorV3.address),
+          chainLinkOracleAsAlice.setPriceFeed(token0.address, token1.address, mockAggregatorV3T0T1.address),
         ).to.revertedWith('Ownable: caller is not the owner')
       })
     })
     context('when the caller is the owner', async () => {
+      context('when source on existed pair', async () => {
+        it('should be reverted', async () => {
+          await chainLinkOracleAsDeployer.setPriceFeed(token0.address, token1.address, mockAggregatorV3T0T1.address)
+          await expect(
+            chainLinkOracleAsDeployer.setPriceFeed(token1.address, token0.address, mockAggregatorV3T1T0.address),
+          ).to.revertedWith('ChainLinkPriceOracle::setPriceFeed:: source on existed pair')
+        })
+      })
       context('when successfully', async () => {
         it('should successfully', async () => {
           await expect(
-            chainLinkOracleAsDeployer.setPriceFeed(token0.address, token1.address, mockAggregatorV3.address),
+            chainLinkOracleAsDeployer.setPriceFeed(token0.address, token1.address, mockAggregatorV3T0T1.address),
           ).to.emit(chainLinkOracleAsDeployer, 'SetPriceFeed')
           const source = await chainLinkOracleAsDeployer.priceFeeds(token0.address, token1.address)
-          expect(source).to.eq(mockAggregatorV3.address)
+          expect(source).to.eq(mockAggregatorV3T0T1.address)
         })
       })
     })
@@ -89,15 +100,15 @@ describe('ChainLinkPriceOracle', () => {
     })
     context('when successfully', async () => {
       it('should successfully', async () => {
-        await chainLinkOracleAsDeployer.setPriceFeed(token0.address, token1.address, mockAggregatorV3.address)
+        await chainLinkOracleAsDeployer.setPriceFeed(token0.address, token1.address, mockAggregatorV3T0T1.address)
 
         const [priceT0T1, lastUpdateT0T1] = await chainLinkOracleAsAlice.getPrice(token0.address, token1.address)
-        // result should be (priceT0T1 * 1e18) / (10**decimals) = (36538981280 * 1e18) / (10**8) = 365389812800000000000
-        expect(priceT0T1).to.eq(BigNumber.from('365389812800000000000'))
+        // result should be (priceT0T1 * 1e18) / (10**decimals) = (36500000000 * 1e18) / (10**8) = 365000000000000000000
+        expect(priceT0T1).to.eq(BigNumber.from('365000000000000000000'))
 
         const [priceT1T0, lastUpdateT1T0] = await chainLinkOracleAsDeployer.getPrice(token1.address, token0.address)
-        // result should be (1e18 * 10**decimals) / (priceT0T1) = (1e18 * 10**8) / (36538981280) = 2736803175591982
-        expect(priceT1T0).to.eq(BigNumber.from('2736803175591982'))
+        // result should be (1e18 * 10**decimals) / (priceT0T1) = (1e18 * 10**8) / (36500000000) = 2739726027397260
+        expect(priceT1T0).to.eq(BigNumber.from('2739726027397260'))
       })
     })
   })
