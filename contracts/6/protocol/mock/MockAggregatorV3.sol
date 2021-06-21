6085377b2f7a6bfbbe9ea348d0af22c9cf0d68a4
jakeal-papaca <jakeal.papaca@gmail.com>
2021-06-21 11:51:42 +0700

    fix: add header SPDX license

--- contracts/6/protocol/mock/MockAggregatorV3.sol
+++ contracts/6/protocol/mock/MockAggregatorV3.sol
@@ -1,3 +1,16 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
 pragma solidity 0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
