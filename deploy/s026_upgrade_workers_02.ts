7b59c718a5a2f72cc6940b46fbbb72d833cb0737
abraham-alpaca <abraham.alpaca@gmail.com>
2021-06-30 02:23:17 +0700

    chore: add an upgrade and a set treasury fields deployment scripts

---
+++ deploy/s026_upgrade_workers_02.ts
@@ -0,0 +1,113 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers, upgrades, network } from 'hardhat';
+import { Timelock__factory, PancakeswapV2Worker, PancakeswapV2Worker__factory, PancakeswapV2WorkerMigrate, PancakeswapV2WorkerMigrate__factory, WaultSwapWorker02__factory, CakeMaxiWorker02__factory, PancakeswapV2Worker02__factory } from '../typechain'
+import MainnetConfig from '../.mainnet.json'
+import TestnetConfig from '../.testnet.json'
+import { ContractFactory } from 'ethers';
+
+
+interface IWorker {
+  WORKER_NAME: string,
+  ADDRESS: string
+  FACTORY: ContractFactory
+}
+
+type IWorkers = Array<IWorker>
+
+interface IFactory {
+  PANCAKESWAP_V2_WORKER_02: PancakeswapV2Worker02__factory
+  WAULTSWAP_WORKER_02: WaultSwapWorker02__factory
+  CAKEMAXI_WORKER_02: CakeMaxiWorker02__factory
+}
+/**
+ *
+ * @description This is a function for getting ContractFactory that is either PancakeswapV2Worker02__factory or WaultSwapWorker02__factory or CakeMaxiWorker02__factory
+ * so that each worker will contain a contract factory using for upgrade proxy
+ * @param {string} workerName
+ * @param {IFactory} factory
+ * @return {*}  {ContractFactory}
+ */
+const getFactory = (workerName: string, factory: IFactory ): ContractFactory => {
+  if (workerName.includes('CakeMaxiWorker')) {
+    return factory.CAKEMAXI_WORKER_02
+  }
+  if (workerName.includes('PancakeswapWorker')) {
+    return factory.PANCAKESWAP_V2_WORKER_02
+  }
+  if (workerName.includes('WaultswapWorker')) {
+    return factory.WAULTSWAP_WORKER_02
+  }
+  throw new Error(`getFactory:: unable to return a factor regarding to the worker ${workerName}`)
+}
+
+
+
+/**
+ * @description Deployment script for upgrades workers to 02 version
+ * @param  {HardhatRuntimeEnvironment} hre
+ */
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  // PROXY_ADMIN
+  // Testnet: 0x2c6c09b46d00A88161B7e4AcFaFEc58990548aC2
+  // Mainnet: 0x5379F32C8D5F663EACb61eeF63F722950294f452
+  const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig
+  const PROXY_ADMIN = config.ProxyAdmin
+  const [pancakeSwapV2Worker02Factory, waultSwapWorker02Factory, cakeMaxiWorker02Factory] = await Promise.all([
+    (await ethers.getContractFactory('PancakeswapV2Worker02')) as PancakeswapV2Worker02__factory,
+    (await ethers.getContractFactory('WaultSwapWorker02')) as WaultSwapWorker02__factory,
+    (await ethers.getContractFactory('CakeMaxiWorker02')) as CakeMaxiWorker02__factory,
+  ])
+  
+  const FACTORY: IFactory = {
+    PANCAKESWAP_V2_WORKER_02: pancakeSwapV2Worker02Factory,
+    WAULTSWAP_WORKER_02: waultSwapWorker02Factory,
+    CAKEMAXI_WORKER_02: cakeMaxiWorker02Factory,
+  }
+  const TO_BE_UPGRADE_WORKERS: IWorkers = config.Vaults.reduce((accum, vault) => {
+    const workers = vault.workers.map((worker) => {
+      return {
+        WORKER_NAME: worker.name,
+        ADDRESS: worker.address,
+        FACTORY: getFactory(worker.name, FACTORY)
+      }
+    })
+    return accum.concat(workers) as IWorkers
+  }, [] as IWorkers)
+  const TIMELOCK = config.Timelock;
+  const EXACT_ETA = '1620575100';
+
+
+
+
+
+
+  const timelock = Timelock__factory.connect(TIMELOCK, (await ethers.getSigners())[0]);
+
+  for(let i = 0; i < TO_BE_UPGRADE_WORKERS.length; i++) {
+    console.log(`>> Upgrading worker: ${TO_BE_UPGRADE_WORKERS[i].WORKER_NAME} at ${TO_BE_UPGRADE_WORKERS[i].ADDRESS} through Timelock + ProxyAdmin`)
+    const preparedNewWorker = await upgrades.prepareUpgrade(TO_BE_UPGRADE_WORKERS[i].ADDRESS, TO_BE_UPGRADE_WORKERS[i].FACTORY)
+    console.log(`>> New implementation deployed at: ${preparedNewWorker}`);
+    console.log("✅ Done");
+
+    console.log(`>> Queue tx on Timelock to upgrade the implementation`);
+    await timelock.queueTransaction(PROXY_ADMIN, '0', 'upgrade(address,address)', ethers.utils.defaultAbiCoder.encode(['address','address'], [TO_BE_UPGRADE_WORKERS[i].ADDRESS, preparedNewWorker]), EXACT_ETA, { gasPrice: 100000000000 });
+    console.log("✅ Done");
+
+    console.log(`>> Generate executeTransaction:`);
+    console.log(`await timelock.executeTransaction('${PROXY_ADMIN}', '0', 'upgrade(address,address)', ethers.utils.defaultAbiCoder.encode(['address','address'], ['${TO_BE_UPGRADE_WORKERS[i].ADDRESS}','${preparedNewWorker}']), ${EXACT_ETA})`);
+    console.log("✅ Done");
+  }
+};
+
+export default func;
+func.tags = ['UpgradeWorkers02'];
---
+++ deploy/s027_timelock_add_treasury_fields_workers_02.ts
@@ -0,0 +1,75 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers, upgrades, network } from 'hardhat';
+import { Timelock__factory, PancakeswapV2Worker, PancakeswapV2Worker__factory, PancakeswapV2WorkerMigrate, PancakeswapV2WorkerMigrate__factory, WaultSwapWorker02__factory, CakeMaxiWorker02__factory, PancakeswapV2Worker02__factory } from '../typechain'
+import MainnetConfig from '../.mainnet.json'
+import TestnetConfig from '../.testnet.json'
+
+interface IWorker {
+  WORKER_NAME: string,
+  ADDRESS: string
+}
+
+type IWorkers = Array<IWorker>
+
+/**
+ * @description Deployment script for upgrades workers to 02 version
+ * @param  {HardhatRuntimeEnvironment} hre
+ */
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig
+  const TO_BE_UPGRADE_WORKERS: IWorkers = config.Vaults.reduce((accum, vault) => {
+    const workers = vault.workers.map((worker) => {
+      return {
+        WORKER_NAME: worker.name,
+        ADDRESS: worker.address,
+      }
+    })
+    return accum.concat(workers) as IWorkers
+  }, [] as IWorkers)
+  const TIMELOCK = config.Timelock;
+  const EXACT_ETA = '1620575100';
+  const TREASURY_ACCOUNT = ''; // Address of treasury account
+  const TREASURY_BOUNTY_BPS = ''; // Treasury bounty bps
+
+
+
+
+
+
+  const timelock = Timelock__factory.connect(TIMELOCK, (await ethers.getSigners())[0]);
+
+  for(let i = 0; i < TO_BE_UPGRADE_WORKERS.length; i++) {
+    console.log(`>> Setting Treasury account to: ${TO_BE_UPGRADE_WORKERS[i].WORKER_NAME} at ${TO_BE_UPGRADE_WORKERS[i].ADDRESS} through Timelock + ProxyAdmin`)
+    console.log(`>> Queue tx on Timelock to upgrade the implementation`);
+    await timelock.queueTransaction(TO_BE_UPGRADE_WORKERS[i].ADDRESS, '0', 'setTreasuryAccount(address)', ethers.utils.defaultAbiCoder.encode(['address'], [TREASURY_ACCOUNT]), EXACT_ETA, { gasPrice: 100000000000 });
+    console.log("✅ Done");
+
+    console.log(`>> Generate executeTransaction:`);
+    console.log(`await timelock.executeTransaction('${TO_BE_UPGRADE_WORKERS[i].ADDRESS}', '0', 'setTreasuryAccount(address)', ethers.utils.defaultAbiCoder.encode(['address'], ['${TREASURY_ACCOUNT}']), ${EXACT_ETA})`);
+    console.log("✅ Done");
+  }
+
+  for(let i = 0; i < TO_BE_UPGRADE_WORKERS.length; i++) {
+    console.log(`>> Setting Treasury bounty Bps to: ${TO_BE_UPGRADE_WORKERS[i].WORKER_NAME} at ${TO_BE_UPGRADE_WORKERS[i].ADDRESS} through Timelock + ProxyAdmin`)
+    console.log(`>> Queue tx on Timelock to upgrade the implementation`);
+    await timelock.queueTransaction(TO_BE_UPGRADE_WORKERS[i].ADDRESS, '0', 'setTreasuryBountyBps(uint256)', ethers.utils.defaultAbiCoder.encode(['uint256'], [TREASURY_BOUNTY_BPS]), EXACT_ETA, { gasPrice: 100000000000 });
+    console.log("✅ Done");
+
+    console.log(`>> Generate executeTransaction:`);
+    console.log(`await timelock.executeTransaction('${TO_BE_UPGRADE_WORKERS[i].ADDRESS}', '0', 'setTreasuryBountyBps(uint256)', ethers.utils.defaultAbiCoder.encode(['uint256'], ['${TREASURY_BOUNTY_BPS}']), ${EXACT_ETA})`);
+    console.log("✅ Done");
+  }
+};
+
+export default func;
+func.tags = ['TimelockAddTreasuryFieldsWorkers02'];
