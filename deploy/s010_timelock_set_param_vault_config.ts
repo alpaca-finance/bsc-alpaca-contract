ea4ac91333f48e48116253d469f04f9a3a2a1fc4
spicysquid168 <spicysquid168@gmail.com>
2021-07-12 11:16:26 +0700

    deploy: triple slope 1.0.3

--- deploy/s010_timelock_set_param_vault_config.ts
+++ deploy/s010_timelock_set_param_vault_config.ts
@@ -1,7 +1,22 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { ethers } from 'hardhat';
-import { IWorker__factory, IStrategy__factory, Timelock__factory } from '../typechain'
+import { ethers, network } from 'hardhat';
+import { Timelock__factory } from '../typechain'
+import MainnetConfig from '../.mainnet.json'
+import TestnetConfig from '../.testnet.json'
+
+interface IInput {
+  VAULT_SYMBOL: string
+  MIN_DEBT_SIZE: string
+  RESERVE_POOL_BPS: string
+  KILL_PRIZE_BPS: string
+  INTEREST_MODEL: string
+  EXACT_ETA: string
+}
+
+interface IInfo extends IInput {
+  VAULT_CONFIG: string
+}
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
@@ -14,30 +29,48 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const NEW_PARAMS = [{
-    VAULT_NAME: 'BNB',
-    VAULT_CONFIG: '0x53dbb71303ad0F9AFa184B8f7147F9f12Bb5Dc01',
-    MIN_DEBT_SIZE: ethers.utils.parseEther('2'),
+  const NEW_PARAMS: Array<IInput> = [{
+    VAULT_SYMBOL: 'ibWBNB',
+    MIN_DEBT_SIZE: '0.2',
     RESERVE_POOL_BPS: '1000',
     KILL_PRIZE_BPS: '500',
-    INTEREST_MODEL: '0x375D32FadA30d7e6Fea242FCa221a22CC6d52B30',
-    WBNB: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    WNATIVE_RELAYER: '0xE1D2CA01bc88F325fF7266DD2165944f3CAf0D3D',
-    FAIRLAUNCH: '0xA625AB01B08ce023B2a342Dbb12a16f2C8489A8F',
-    TIMELOCK: '0x2D5408f2287BF9F9B05404794459a846651D0a59',
-    EXACT_ETA: '1617926400',
+    INTEREST_MODEL: '0x111ae8da53D0998260DfdfA2172d4f88f969d386',
+    EXACT_ETA: '1626063180',
   }, {
-    VAULT_NAME: 'BUSD',
-    VAULT_CONFIG: '0xd7b805E88c5F52EDE71a9b93F7048c8d632DBEd4',
-    MIN_DEBT_SIZE: ethers.utils.parseEther('400'),
+    VAULT_SYMBOL: 'ibBUSD',
+    MIN_DEBT_SIZE: '100',
     RESERVE_POOL_BPS: '1000',
     KILL_PRIZE_BPS: '500',
-    INTEREST_MODEL: '0x375D32FadA30d7e6Fea242FCa221a22CC6d52B30',
-    WBNB: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    WNATIVE_RELAYER: '0xE1D2CA01bc88F325fF7266DD2165944f3CAf0D3D',
-    FAIRLAUNCH: '0xA625AB01B08ce023B2a342Dbb12a16f2C8489A8F',
-    TIMELOCK: '0x2D5408f2287BF9F9B05404794459a846651D0a59',
-    EXACT_ETA: '1617926400',
+    INTEREST_MODEL: '0x111ae8da53D0998260DfdfA2172d4f88f969d386',
+    EXACT_ETA: '1626063180',
+  }, {
+    VAULT_SYMBOL: 'ibETH',
+    MIN_DEBT_SIZE: '0.04',
+    RESERVE_POOL_BPS: '1000',
+    KILL_PRIZE_BPS: '500',
+    INTEREST_MODEL: '0x111ae8da53D0998260DfdfA2172d4f88f969d386',
+    EXACT_ETA: '1626063180'
+  }, {
+    VAULT_SYMBOL: 'ibALPACA',
+    MIN_DEBT_SIZE: '50',
+    RESERVE_POOL_BPS: '1000',
+    KILL_PRIZE_BPS: '500',
+    INTEREST_MODEL: '0x111ae8da53D0998260DfdfA2172d4f88f969d386',
+    EXACT_ETA: '1626063180'
+  }, {
+    VAULT_SYMBOL: 'ibUSDT',
+    MIN_DEBT_SIZE: '100',
+    RESERVE_POOL_BPS: '1000',
+    KILL_PRIZE_BPS: '500',
+    INTEREST_MODEL: '0x111ae8da53D0998260DfdfA2172d4f88f969d386',
+    EXACT_ETA: '1626063180'
+  }, {
+    VAULT_SYMBOL: 'ibBTCB',
+    MIN_DEBT_SIZE: '0.002',
+    RESERVE_POOL_BPS: '1000',
+    KILL_PRIZE_BPS: '500',
+    INTEREST_MODEL: '0x111ae8da53D0998260DfdfA2172d4f88f969d386',
+    EXACT_ETA: '1626063180'
   }]
 
 
@@ -47,12 +80,28 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 
 
+  const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig
+  const timelock = Timelock__factory.connect(config.Timelock, (await ethers.getSigners())[0]);
 
+  /// @dev derived input
+  const infos: Array<IInfo> = NEW_PARAMS.map((n) => {
+    const vault = config.Vaults.find((v) => v.symbol === n.VAULT_SYMBOL)
+    if(vault === undefined) throw new Error(`error: unable to map ${n.VAULT_SYMBOL} to any vault`)
 
-  for(let i = 0; i < NEW_PARAMS.length; i++) {
-    const timelock = Timelock__factory.connect(NEW_PARAMS[i].TIMELOCK, (await ethers.getSigners())[0]);
+    return {
+      VAULT_SYMBOL: n.VAULT_SYMBOL,
+      VAULT_CONFIG: vault.config,
+      MIN_DEBT_SIZE: n.MIN_DEBT_SIZE,
+      RESERVE_POOL_BPS: n.RESERVE_POOL_BPS,
+      KILL_PRIZE_BPS: n.KILL_PRIZE_BPS,
+      INTEREST_MODEL: n.INTEREST_MODEL,
+      EXACT_ETA: n.EXACT_ETA
+    }
+  })
+
+  for(const info of infos) {
     console.log("===================================================================================")
-    console.log(`>> Queuing transaction to update ${NEW_PARAMS[i].VAULT_NAME} Vault config`);
+    console.log(`>> Queuing transaction to update ${info.VAULT_SYMBOL} Vault config`);
     // function setParams(
     //   uint256 _minDebtSize,
     //   uint256 _reservePoolBps,
@@ -63,14 +112,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     //   address _fairLaunch
     // )
     await timelock.queueTransaction(
-      NEW_PARAMS[i].VAULT_CONFIG, '0',
+      info.VAULT_CONFIG, '0',
       'setParams(uint256,uint256,uint256,address,address,address,address)',
       ethers.utils.defaultAbiCoder.encode(
         ['uint256','uint256','uint256','address','address','address','address'],
-        [NEW_PARAMS[i].MIN_DEBT_SIZE, NEW_PARAMS[i].RESERVE_POOL_BPS, NEW_PARAMS[i].KILL_PRIZE_BPS, NEW_PARAMS[i].INTEREST_MODEL, NEW_PARAMS[i].WBNB, NEW_PARAMS[i].WNATIVE_RELAYER, NEW_PARAMS[i].FAIRLAUNCH]), NEW_PARAMS[i].EXACT_ETA)
+        [ethers.utils.parseEther(info.MIN_DEBT_SIZE), info.RESERVE_POOL_BPS, info.KILL_PRIZE_BPS, info.INTEREST_MODEL, config.Tokens.WBNB, config.SharedConfig.WNativeRelayer, config.FairLaunch.address]), info.EXACT_ETA)
     console.log("✅ Done")
     console.log("timelock execution:");
-    console.log(`await timelock.executeTransaction('${NEW_PARAMS[i].VAULT_CONFIG}', '0', 'setParams(uint256,uint256,uint256,address,address,address,address)', ethers.utils.defaultAbiCoder.encode(['uint256','uint256','uint256','address','address','address','address'],['${NEW_PARAMS[i].MIN_DEBT_SIZE}', '${NEW_PARAMS[i].RESERVE_POOL_BPS}', '${NEW_PARAMS[i].KILL_PRIZE_BPS}', '${NEW_PARAMS[i].INTEREST_MODEL}', '${NEW_PARAMS[i].WBNB}', '${NEW_PARAMS[i].WNATIVE_RELAYER}', '${NEW_PARAMS[i].FAIRLAUNCH}']), ${NEW_PARAMS[i].EXACT_ETA})`);
+    console.log(`await timelock.executeTransaction('${info.VAULT_CONFIG}', '0', 'setParams(uint256,uint256,uint256,address,address,address,address)', ethers.utils.defaultAbiCoder.encode(['uint256','uint256','uint256','address','address','address','address'],['${ethers.utils.parseEther(info.MIN_DEBT_SIZE)}', '${info.RESERVE_POOL_BPS}', '${info.KILL_PRIZE_BPS}', '${info.INTEREST_MODEL}', '${config.Tokens.WBNB}', '${config.SharedConfig.WNativeRelayer}', '${config.FairLaunch.address}']), ${info.EXACT_ETA})`);
   }
 };
 
