22f0437cec92a33f26da9e1ab47a210d18b6546c
Almost Llamas <kim.tony.20.20.20@gmail.com>
2021-08-31 14:20:50 +0700

    chore: import Scientix contract

---
+++ contracts/6.12/protocol/apis/scientix/Import.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩
+  ξ ･×･ ξ
+  ξ　~　ξ
+  ξ　　 ξ
+  ξ　　 “~～~～〇
+  ξ　　　　　　 ξ
+  ξ ξ ξ~～~ξ ξ ξ
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+/**
+  The purpose of this file is to add the following contract to TypeChain compilation
+ */
+
+pragma solidity 0.6.12;
+
+import "@scientix-finance/scientix-contract/contracts/StakingPools.sol";
