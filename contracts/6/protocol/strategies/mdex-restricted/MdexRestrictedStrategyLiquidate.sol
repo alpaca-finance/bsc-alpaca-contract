1227c7170a0c17c40f5b706b4d0b0c75ad58b85c
abc-alpaca <gabcdef637@gmail.com>
2021-08-31 13:42:48 +0700

    feat: add liquidate strategy

---
+++ contracts/6/protocol/strategies/mdex-restricted/MdexRestrictedStrategyLiquidate.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/Math.sol";
+
+import "../../apis/mdex/IMdexRouter.sol";
+
+import "../../apis/mdex/IMdexFactory.sol";
+import "../../apis/mdex/IMdexPair.sol";
+
+import "../../interfaces/IStrategy.sol";
+import "../../interfaces/IWorker.sol";
+
+import "../../../utils/SafeToken.sol";
+
+contract MdexRestrictedStrategyLiquidate is OwnableUpgradeSafe, ReentrancyGuardUpgradeSafe, IStrategy {
+  using SafeToken for address;
+  using SafeMath for uint256;
+
+  IMdexFactory public factory;
+  IMdexRouter public router;
+
+  mapping(address => bool) public okWorkers;
+
+  /// @notice require that only allowed workers are able to do the rest of the method call
+  modifier onlyWhitelistedWorkers() {
+    require(okWorkers[msg.sender], "MdexRestrictedStrategyLiquidate::onlyWhitelistedWorkers:: bad worker");
+    _;
+  }
+
+  /// @dev Create a new liquidate strategy instance.
+  /// @param _router The IMdexRouter  smart contract.
+  function initialize(IMdexRouter _router) external initializer {
+    OwnableUpgradeSafe.__Ownable_init();
+    ReentrancyGuardUpgradeSafe.__ReentrancyGuard_init();
+    factory = IMdexFactory(_router.factory());
+    router = _router;
+  }
+
+  /// @dev Execute worker strategy. Take LP token. Return  BaseToken.
+  /// @param data Extra calldata information passed along to this strategy.
+  function execute(
+    address, /* user */
+    uint256 debt,
+    bytes calldata data
+  ) external override onlyWhitelistedWorkers nonReentrant {
+    // 1. Find out what farming token we are dealing with.
+    uint256 minBaseToken = abi.decode(data, (uint256));
+    IWorker worker = IWorker(msg.sender);
+    address baseToken = worker.baseToken();
+    address farmingToken = worker.farmingToken();
+
+    //TODO double check
+    IMdexPair lpToken = IMdexPair(factory.getPair(farmingToken, baseToken));
+    // 2. Approve router to do their stuffs
+    address(lpToken).safeApprove(address(router), uint256(-1));
+    farmingToken.safeApprove(address(router), uint256(-1));
+    // 3. Remove all liquidity back to BaseToken and farming tokens.
+    router.removeLiquidity(baseToken, farmingToken, lpToken.balanceOf(address(this)), 0, 0, address(this), now);
+    // 4. Convert farming tokens to baseToken.
+    address[] memory path = new address[](2);
+    path[0] = farmingToken;
+    path[1] = baseToken;
+    router.swapExactTokensForTokens(farmingToken.myBalance(), 0, path, address(this), now);
+    // 5. Return all baseToken back to the original caller.
+    uint256 balance = baseToken.myBalance();
+    require(
+      balance.sub(debt) >= minBaseToken,
+      "MdexRestrictedStrategyLiquidate::execute:: insufficient baseToken received"
+    );
+    SafeToken.safeTransfer(baseToken, msg.sender, balance);
+    // 6. Reset approve for safety reason
+    address(lpToken).safeApprove(address(router), 0);
+    farmingToken.safeApprove(address(router), 0);
+  }
+
+  function setWorkersOk(address[] calldata workers, bool isOk) external onlyOwner {
+    for (uint256 idx = 0; idx < workers.length; idx++) {
+      okWorkers[workers[idx]] = isOk;
+    }
+  }
+}
