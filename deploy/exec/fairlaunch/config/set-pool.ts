f7095fda64db4bc2714af2f942ce5fdc4e0e9514
spicysquid168 <spicysquid168@gmail.com>
2021-09-01 12:42:32 +0700

    feat: add set pool timelock script

---
+++ deploy/exec/fairlaunch/config/set-pool.ts
@@ -0,0 +1,94 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, network } from "hardhat";
+import { Timelock__factory } from "../../../../typechain";
+import MainnetConfig from "../../../../.mainnet.json";
+import TestnetConfig from "../../../../.testnet.json";
+
+interface IUpdate {
+  STAKING_TOKEN: string;
+  ALLOC_POINT: number;
+}
+
+interface IInput {
+  pId: number;
+  stakingPool: string;
+  allocPoint: number;
+}
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const UPDATES: Array<IUpdate> = [
+    {
+      STAKING_TOKEN: "ibALPACA",
+      ALLOC_POINT: 350,
+    },
+    {
+      STAKING_TOKEN: "ibWBNB",
+      ALLOC_POINT: 225,
+    },
+    {
+      STAKING_TOKEN: "ibBUSD",
+      ALLOC_POINT: 185,
+    },
+    {
+      STAKING_TOKEN: "ibUSDT",
+      ALLOC_POINT: 100,
+    },
+    {
+      STAKING_TOKEN: "ibTUSD",
+      ALLOC_POINT: 60,
+    },
+    {
+      STAKING_TOKEN: "debtIbALPACA V2",
+      ALLOC_POINT: 5,
+    },
+  ];
+  const EXACT_ETA = "1630561500";
+
+  const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig;
+  const inputs: Array<IInput> = [];
+
+  /// @dev derived input
+  for (let i = 0; i < UPDATES.length; i++) {
+    const pool = config.FairLaunch.pools.find((p) => p.stakingToken == UPDATES[i].STAKING_TOKEN);
+    if (pool !== undefined) {
+      inputs.push({
+        pId: pool.id,
+        stakingPool: pool.stakingToken,
+        allocPoint: UPDATES[i].ALLOC_POINT,
+      });
+    } else {
+      throw new Error(`not found ${UPDATES[i].STAKING_TOKEN}`);
+    }
+  }
+
+  const timelock = Timelock__factory.connect(config.Timelock, (await ethers.getSigners())[0]);
+
+  for (const input of inputs) {
+    console.log(`>> Timelock: Set pool for ${input.stakingPool} via Timelock`);
+    await timelock.queueTransaction(
+      config.Shield,
+      "0",
+      "setPool(uint256,uint256,bool)",
+      ethers.utils.defaultAbiCoder.encode(["uint256", "uint256", "bool"], [input.pId, input.allocPoint, true]),
+      EXACT_ETA
+    );
+    console.log("generate timelock.executeTransaction:");
+    console.log(
+      `await timelock.executeTransaction('${config.Shield}', '0', 'setPool(uint256,uint256,bool)', ethers.utils.defaultAbiCoder.encode(['uint256','uint256','bool'],[${input.pId}, ${input.allocPoint}, true]), ${EXACT_ETA})`
+    );
+    console.log("✅ Done");
+  }
+};
+
+export default func;
+func.tags = ["TimelockSetPool"];
