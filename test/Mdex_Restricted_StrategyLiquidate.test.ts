e38fe0e48d6fb60abed84ae49aac7725d526731d
abc-alpaca <gabcdef637@gmail.com>
2021-09-02 14:51:55 +0700

    feat: initial first case unit test

--- test/Mdex_Restricted_StrategyLiquidate.test.ts
+++ test/Mdex_Restricted_StrategyLiquidate.test.ts
@@ -3,7 +3,7 @@ import { Signer, BigNumberish, utils, Wallet } from "ethers";
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import "@openzeppelin/test-helpers";
-import { MdexFactory, MdexFactory__factory, MdexPair, MdexPair__factory, MdexRouter, MdexRouter__factory, MockERC20, MockERC20__factory, WETH, WETH__factory } from "../typechain";
+import { MdexFactory, MdexFactory__factory, MdexPair, MdexPair__factory,MdexRestrictedStrategyLiquidate, MdexRestrictedStrategyLiquidate__factory, MdexRouter, MdexRouter__factory, MockERC20, MockERC20__factory, MockMdexWorker, MockMdexWorker__factory, WETH, WETH__factory } from "../typechain";
 import { deploy } from "@openzeppelin/hardhat-upgrades/dist/utils";
 
 chai.use(solidity);
@@ -13,36 +13,66 @@ const { expect } = chai;
 describe("MdexRestricted_StrategyLiquidate",()=>{
 
 // FIXME do declare variable
-    let factory: MdexFactory
-    let router: MdexRouter
-    let mdexPair: MdexPair
+    let factory: MdexFactory;
+    let router: MdexRouter;
+    let mdexPair: MdexPair;
     
     let wbnb : WETH;
-    let baseToken: MockERC20
-    let farmingToken : MockERC20 
+    let baseToken: MockERC20;
+    let farmingToken : MockERC20; 
+    let mdexToken : MockERC20;
+
+    let mockMdexWorker : MockMdexWorker;
+    let mockMdexEvilWorker : MockMdexWorker;
+    
+    let strat : MdexRestrictedStrategyLiquidate;
+
+// initial Contract Signer part
+    let deployer: Signer
+    let alice: Signer
+    let bob: Signer
+
+    let baseTokenAsAlice : MockERC20
+    let baseTokenAsBob : MockERC20
+
+    let farmingTokenAsAlice: MockERC20
+    let farmingTokenAsBob: MockERC20
+
+    let wbnbTokenAsAlice: WETH
+
+    let routerAsAlice: MdexRouter
+    let routerAsBob: MdexRouter
+
+    let stratAsAlice : MdexRestrictedStrategyLiquidate
+    let stratAsBob : MdexRestrictedStrategyLiquidate
+
+    
 
     async function fixture(){
     //FIXME do initial stuff
-    const [deployer,alice,bob] = await ethers.getSigners();
+     [deployer,alice,bob] = await ethers.getSigners();
 
-    const MdexFactory = (await ethers.getContractFactory('MdexFactory',deployer)) as MdexFactory__factory;
+    const MdexFactory = (await ethers.getContractFactory("MdexFactory",deployer)) as MdexFactory__factory;
     factory = await MdexFactory.deploy(await deployer.getAddress());
     await factory.deployed();
 
-    const WBNB = (await ethers.getContractFactory('WETH',deployer)) as WETH__factory;
+    const WBNB = (await ethers.getContractFactory("WETH",deployer)) as WETH__factory;
     wbnb = await WBNB.deploy();
     await wbnb.deployed();
 
-    const MdexRouter = (await ethers.getContractFactory('MdexRouter',deployer)) as MdexRouter__factory;
+    const MdexRouter = (await ethers.getContractFactory("MdexRouter",deployer)) as MdexRouter__factory;
     router = await MdexRouter.deploy(factory.address,wbnb.address)
     await router.deployed()
 
-    const MockERC20 = (await ethers.getContractFactory('MockERC20',deployer)) as MockERC20__factory;
+    const MockERC20 = (await ethers.getContractFactory("MockERC20",deployer)) as MockERC20__factory;
     baseToken = (await upgrades.deployProxy(MockERC20, ["BTOKEN", "BTOKEN"])) as MockERC20;
     await baseToken.deployed();
     
     farmingToken = (await upgrades.deployProxy(MockERC20,["FTOKEN","FTOKEN"])) as MockERC20;
     await farmingToken.deployed();
+
+    mdexToken = (await upgrades.deployProxy(MockERC20,["MTOKEN","MTOKEN"])) as MockERC20;
+    await mdexToken.deployed();
 
     // mint to alice and bob 100 BToken
     await baseToken.mint(await alice.getAddress(),ethers.utils.parseEther("100"))
@@ -54,9 +84,35 @@ describe("MdexRestricted_StrategyLiquidate",()=>{
     //create pair
     await factory.createPair(baseToken.address,farmingToken.address)
 
-    
+    // create LP for using at worker
     mdexPair = MdexPair__factory.connect(await factory.getPair(baseToken.address,farmingToken.address),deployer)
     
+    const MockMdexWorker = (await ethers.getContractFactory("MockMdexWorker",deployer)) as MockMdexWorker__factory;
+
+    mockMdexWorker = (await MockMdexWorker.deploy(
+        mdexPair.address,
+        baseToken.address,
+        farmingToken.address
+    )) as MockMdexWorker;
+
+    await mockMdexWorker.deployed();
+
+    mockMdexEvilWorker = (await MockMdexWorker.deploy(
+        mdexPair.address,
+        baseToken.address,
+        farmingToken.address
+    )) as MockMdexWorker;
+
+    await mockMdexEvilWorker.deployed();
+
+    const MdexRestrictedStrategyLiquidate = (await ethers.getContractFactory("MdexRestrictedStrategyLiquidate",deployer))as MdexRestrictedStrategyLiquidate__factory;
+    strat = (await upgrades.deployProxy(MdexRestrictedStrategyLiquidate, [router.address,mdexToken.address])) as 
+    MdexRestrictedStrategyLiquidate;
+    
+    await strat.deployed();
+    await strat.setWorkersOk([mockMdexWorker.address],true);
+
+    stratAsBob = MdexRestrictedStrategyLiquidate__factory.connect(strat.address,bob);
 
 
     }
@@ -67,8 +123,7 @@ describe("MdexRestricted_StrategyLiquidate",()=>{
 
     context("When bad calldata", async () => {
     it("should revert", async () => {
-        // Bob passes some bad calldata that can't be decoded
-        // await expect(stratAsBob.execute(await bob.getAddress(), "0", "0x1234")).to.be.reverted;
+        await expect(stratAsBob.execute(await bob.getAddress(),"0","0x1234")).to.be.reverted;
      });
     });
 })
