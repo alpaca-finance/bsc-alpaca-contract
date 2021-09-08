43a64cbaf47c4fb351ce4d1b553cf827dad779c5
jr-alpaca <uviveramrom@gmail.com>
2021-09-08 19:31:43 +0700

    feat: add-mdex-partialCloseLiquidate-strategy move import IMdexSwapMining to the mdex import group

--- contracts/6/protocol/strategies/mdex-restricted/MdexRestrictedStrategyPartialCloseLiquidate.sol
+++ contracts/6/protocol/strategies/mdex-restricted/MdexRestrictedStrategyPartialCloseLiquidate.sol
@@ -20,11 +20,11 @@ import "@openzeppelin/contracts-ethereum-package/contracts/math/Math.sol";
 
 import "../../apis/mdex/IMdexFactory.sol";
 import "../../apis/mdex/IMdexRouter.sol";
+import "../../interfaces/IMdexSwapMining.sol";
 
 import "../../interfaces/IWorker.sol";
 import "../../interfaces/IStrategy.sol";
 import "../../interfaces/IVault.sol";
-import "../../interfaces/ISwapMining.sol";
 
 import "../../../utils/SafeToken.sol";
 
@@ -116,7 +116,7 @@ contract MdexRestrictedStrategyPartialCloseLiquidate is OwnableUpgradeSafe, Reen
   /// @dev Withdraw trading all reward.
   /// @param to The address to transfer trading reward to.
   function withdrawTradingRewards(address to) external onlyOwner {
-    ISwapMining(router.swapMining()).takerWithdraw();
+    IMdexSwapMining(router.swapMining()).takerWithdraw();
     mdx.safeTransfer(to, mdx.myBalance());
   }
 
@@ -126,7 +126,7 @@ contract MdexRestrictedStrategyPartialCloseLiquidate is OwnableUpgradeSafe, Reen
     address swapMiningAddress = router.swapMining();
     uint256 totalReward;
     for (uint256 pid = 0; pid < pIds.length; pid++) {
-      (uint256 reward, ) = ISwapMining(swapMiningAddress).getUserReward(pid);
+      (uint256 reward, ) = IMdexSwapMining(swapMiningAddress).getUserReward(pid);
       totalReward = totalReward.add(reward);
     }
     return totalReward;
