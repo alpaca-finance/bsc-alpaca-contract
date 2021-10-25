17d9fb25e20ce3d8780638661ed638e03a816d45
spicysquid168 <spicysquid168@gmail.com>
2021-10-25 13:04:06 +0700

    chore: add missing tokens

--- deploy/exec/oracles/config/add-primary-source-oracle-medianizer.ts
+++ deploy/exec/oracles/config/add-primary-source-oracle-medianizer.ts
@@ -18,11 +18,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const DEFAULT_MAX_PRICE_STALE = "86400";
   const config = ConfigEntity.getConfig();
 
-  const TOKEN0_SYMBOLS = ["ALPACA", "ALPACA"];
-  const TOKEN1_SYMBOLS = ["BUSD", "USDT"];
-  const MAX_PRICE_DEVIATIONS = [DEFAULT_MAX_PRICE_DEVIATION, DEFAULT_MAX_PRICE_DEVIATION];
-  const MAX_PRICE_STALES = [DEFAULT_MAX_PRICE_STALE, DEFAULT_MAX_PRICE_STALE];
-  const SOURCES = [[config.Oracle.ChainLinkOracle], [config.Oracle.ChainLinkOracle]];
+  const TOKEN0_SYMBOLS = ["BMON"];
+  const TOKEN1_SYMBOLS = ["BUSD"];
+  const MAX_PRICE_DEVIATIONS = [DEFAULT_MAX_PRICE_DEVIATION];
+  const MAX_PRICE_STALES = [DEFAULT_MAX_PRICE_STALE];
+  const SOURCES = [[config.Oracle.SimpleOracle]];
 
   const tokenList: any = config.Tokens;
   const token0Addrs: Array<string> = TOKEN0_SYMBOLS.map((t) => {
