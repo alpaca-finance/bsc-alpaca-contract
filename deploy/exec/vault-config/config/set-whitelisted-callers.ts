a96053738d419253df7bbefa8e25344efc5b4dbd
spicysquid168 <spicysquid168@gmail.com>
2022-01-04 10:54:21 +0700

    feat: add emission adjust script

--- deploy/exec/vault-config/config/set-whitelisted-callers.ts
+++ deploy/exec/vault-config/config/set-whitelisted-callers.ts
@@ -32,41 +32,41 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const TARGETED_VAULT_CONFIG: Array<IInput> = [
     {
       VAULT_SYMBOL: "ibWBNB",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
     {
       VAULT_SYMBOL: "ibBUSD",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
     {
       VAULT_SYMBOL: "ibETH",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
     {
       VAULT_SYMBOL: "ibALPACA",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
     {
       VAULT_SYMBOL: "ibUSDT",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
     {
       VAULT_SYMBOL: "ibBTCB",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
     {
       VAULT_SYMBOL: "ibTUSD",
-      WHITELISTED_CALLERS: ["0x30903486f9b438c4dd0Db38bf8C7dF9B193971F9"],
+      WHITELISTED_CALLERS: ["0x183f6FB3D045F5786e58D75C1b66b4179e64a45c"],
       IS_ENABLE: true,
     },
   ];
-  const EXACT_ETA = "1639989000";
+  const EXACT_ETA = "1640862360";
 
   const config = network.name === "mainnet" ? MainnetConfig : TestnetConfig;
   const timelockTransactions: Array<TimelockEntity.Transaction> = [];
