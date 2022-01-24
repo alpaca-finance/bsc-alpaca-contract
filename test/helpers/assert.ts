ab7d38c1938216deb0de8d38e58a213bcafe35df
abc-alpaca <gabcdef637@gmail.com>
2022-01-24 10:42:52 +0700

    test: add test

--- test/helpers/assert.ts
+++ test/helpers/assert.ts
@@ -1,14 +1,34 @@
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
-import { BigNumber } from "ethers";
+import { BigNumber, ethers } from "ethers";
 
 chai.use(solidity);
-const { expect } = chai;
+const { expect, assert } = chai;
 
 export function assertAlmostEqual(expected: string, actual: string) {
   const expectedBN = BigNumber.from(expected);
   const actualBN = BigNumber.from(actual);
   const diffBN = expectedBN.gt(actualBN) ? expectedBN.sub(actualBN) : actualBN.sub(expectedBN);
-  const tolerance = expectedBN.div(BigNumber.from('10000'))
+  const tolerance = expectedBN.div(BigNumber.from("10000"));
   return expect(diffBN, `${actual} is not almost eqaual to ${expected}`).to.be.lte(tolerance);
+}
+
+export function assertBigNumberClosePercent(
+  a: ethers.BigNumberish,
+  b: ethers.BigNumberish,
+  variance = "0.02",
+  reason = ""
+): void {
+  const aBigNumber = ethers.BigNumber.from(a);
+  const bBigNumber = ethers.BigNumber.from(b);
+  const varianceBigNumber = ethers.utils.parseUnits(variance, 16);
+
+  if (aBigNumber.eq(bBigNumber)) return;
+
+  const diff = aBigNumber.sub(bBigNumber).abs().mul(ethers.constants.WeiPerEther).div(aBigNumber.add(bBigNumber));
+  console.log("varianceBigNumber", varianceBigNumber.toString());
+  console.log("diff", diff.toString());
+  300000000000000;
+  9559612654976426;
+  assert.ok(diff.lte(varianceBigNumber), `${reason}: diff exceeded ${variance}`);
 }
--- test/integrations/protocol/PriceHelper.test.ts
+++ test/integrations/protocol/PriceHelper.test.ts
@@ -10,8 +10,6 @@ import {
   MockAggregatorV3__factory,
   MockERC20,
   MockERC20__factory,
-  OracleMedianizer,
-  OracleMedianizer__factory,
   PancakeFactory,
   PancakeFactory__factory,
   PancakePair,
@@ -20,19 +18,15 @@ import {
   PancakeRouterV2__factory,
   PriceHelper,
   PriceHelper__factory,
-  SimplePriceOracle,
-  SimplePriceOracle__factory,
   WETH,
   WETH__factory,
 } from "../../../typechain";
-import * as TimeHelpers from "../../helpers/time";
-import { deploy } from "@openzeppelin/hardhat-upgrades/dist/utils";
+import { assertBigNumberClosePercent } from "../../helpers/assert";
 
 chai.use(solidity);
 const { expect } = chai;
 
 const FOREVER = "2000000000";
-const ADDRESS0 = "0x0000000000000000000000000000000000000000";
 
 // Accounts
 let deployer: Signer;
@@ -42,11 +36,13 @@ let factoryV2: PancakeFactory;
 let routerV2: PancakeRouter;
 let lpV2Token0Stable0: PancakePair;
 let lpV2Stable0Stable1: PancakePair;
+let lpV2BtcbStable0: PancakePair;
+let lpV2BtcbToken0: PancakePair;
 
 /// Token-related instance(s)
 let wbnb: WETH;
 let token0: MockERC20;
-let token1: MockERC20;
+let btcbToken: MockERC20;
 let usdToken: MockERC20;
 let stableToken0: MockERC20;
 let stableToken1: MockERC20;
@@ -55,13 +51,12 @@ let chainLinkOracle: ChainLinkPriceOracle;
 let chainLinkOracleAsDeployer: ChainLinkPriceOracle;
 
 let mockAggregatorV3T0UsdToken: MockAggregatorV3;
-
-let mockAggregatorV3T1UsdToken: MockAggregatorV3;
+let mockAggregatorV3BtcbUsdToken: MockAggregatorV3;
 let mockAggregatorV3StableToken: MockAggregatorV3;
 
-let mockAggregatorV3T0T1Token: MockAggregatorV3;
-
 let priceHelper: PriceHelper;
+
+let casesData: any[] = [];
 
 async function fixture() {
   [deployer] = await ethers.getSigners();
@@ -72,8 +67,8 @@ async function fixture() {
   const ERC20 = (await ethers.getContractFactory("MockERC20", deployer)) as MockERC20__factory;
   token0 = (await upgrades.deployProxy(ERC20, ["token0", "token0", "18"])) as MockERC20;
   await token0.deployed();
-  token1 = (await upgrades.deployProxy(ERC20, ["token1", "token1", "8"])) as MockERC20;
-  await token1.deployed();
+  btcbToken = (await upgrades.deployProxy(ERC20, ["btcb", "btcb", "18"])) as MockERC20;
+  await btcbToken.deployed();
   usdToken = (await upgrades.deployProxy(ERC20, ["usdToken", "usdToken", "18"])) as MockERC20;
   await usdToken.deployed();
 
@@ -85,11 +80,11 @@ async function fixture() {
 
   const MockAggregatorV3 = (await ethers.getContractFactory("MockAggregatorV3", deployer)) as MockAggregatorV3__factory;
 
-  mockAggregatorV3T0UsdToken = await MockAggregatorV3.deploy(BigNumber.from("468290000000000000000"), 18);
+  mockAggregatorV3T0UsdToken = await MockAggregatorV3.deploy(BigNumber.from("427840000000000000000"), 18);
   await mockAggregatorV3T0UsdToken.deployed();
 
-  mockAggregatorV3T1UsdToken = await MockAggregatorV3.deploy(BigNumber.from("10000000000000000000"), 18);
-  await mockAggregatorV3T1UsdToken.deployed();
+  mockAggregatorV3BtcbUsdToken = await MockAggregatorV3.deploy(BigNumber.from("42300713817358000000000"), 18);
+  await mockAggregatorV3BtcbUsdToken.deployed();
 
   mockAggregatorV3StableToken = await MockAggregatorV3.deploy(BigNumber.from("1000000000000000000"), 18);
   await mockAggregatorV3StableToken.deployed();
@@ -105,11 +100,11 @@ async function fixture() {
 
   // feed price on oracle
   await chainLinkOracleAsDeployer.setPriceFeeds(
-    [token0.address, token1.address, stableToken0.address, stableToken1.address],
+    [token0.address, btcbToken.address, stableToken0.address, stableToken1.address],
     [usdToken.address, usdToken.address, usdToken.address, usdToken.address],
     [
       mockAggregatorV3T0UsdToken.address,
-      mockAggregatorV3T1UsdToken.address,
+      mockAggregatorV3BtcbUsdToken.address,
       mockAggregatorV3StableToken.address,
       mockAggregatorV3StableToken.address,
     ]
@@ -127,27 +122,36 @@ async function fixture() {
   routerV2 = await PancakeRouterV2.deploy(factoryV2.address, wbnb.address);
   await routerV2.deployed();
 
+  // CREATE PAIR
   await factoryV2.createPair(token0.address, stableToken0.address);
   const lpAddress = await factoryV2.getPair(token0.address, stableToken0.address);
 
   await factoryV2.createPair(stableToken0.address, stableToken1.address);
   const stableLPAddress = await factoryV2.getPair(stableToken0.address, stableToken1.address);
 
+  await factoryV2.createPair(btcbToken.address, stableToken0.address);
+  const btcbStable0Address = await factoryV2.getPair(btcbToken.address, stableToken0.address);
+
+  await factoryV2.createPair(btcbToken.address, token0.address);
+  const btcbToken0Address = await factoryV2.getPair(btcbToken.address, token0.address);
+
   lpV2Token0Stable0 = PancakePair__factory.connect(lpAddress, deployer);
   lpV2Stable0Stable1 = PancakePair__factory.connect(stableLPAddress, deployer);
+  lpV2BtcbStable0 = PancakePair__factory.connect(btcbStable0Address, deployer);
+  lpV2BtcbToken0 = PancakePair__factory.connect(btcbToken0Address, deployer);
 
   //ADD LIQUIDITY TOKEN0 STABLE0
-  await token0.mint(deployerAddress, BigNumber.from("409514710336210000000000"));
-  await stableToken0.mint(deployerAddress, BigNumber.from("192119364726297000000000000"));
+  await token0.mint(deployerAddress, BigNumber.from("466712574325720000000000"));
+  await stableToken0.mint(deployerAddress, BigNumber.from("199675089295813000000000000"));
 
-  await token0.approve(routerV2.address, BigNumber.from("409514710336210000000000"));
-  await stableToken0.approve(routerV2.address, BigNumber.from("192119364726297000000000000"));
+  await token0.approve(routerV2.address, BigNumber.from("466712574325720000000000"));
+  await stableToken0.approve(routerV2.address, BigNumber.from("199675089295813000000000000"));
 
   await routerV2.addLiquidity(
     token0.address,
     stableToken0.address,
-    BigNumber.from("409514710336210000000000"),
-    BigNumber.from("192119364726297000000000000"),
+    BigNumber.from("466712574325720000000000"),
+    BigNumber.from("199675089295813000000000000"),
     "0",
     "0",
     deployerAddress,
@@ -172,17 +176,115 @@ async function fixture() {
     FOREVER
   );
 
+  // ADD LIQUIDITY BTCB - BUSD
+  await btcbToken.mint(deployerAddress, BigNumber.from("904717988720000000000"));
+  await stableToken0.mint(deployerAddress, BigNumber.from("38285596932068700000000000"));
+
+  await btcbToken.approve(routerV2.address, BigNumber.from("904717988720000000000"));
+  await stableToken0.approve(routerV2.address, BigNumber.from("38285596932068700000000000"));
+
+  await routerV2.addLiquidity(
+    btcbToken.address,
+    stableToken0.address,
+    BigNumber.from("904717988720000000000"),
+    BigNumber.from("38285596932068700000000000"),
+    "0",
+    "0",
+    deployerAddress,
+    FOREVER
+  );
+
+  // ADD LIQUIDITY BTCB - WBNB
+  await btcbToken.mint(deployerAddress, BigNumber.from("965510000000000000000"));
+  await token0.mint(deployerAddress, BigNumber.from("91512193227770000000000"));
+
+  await btcbToken.approve(routerV2.address, BigNumber.from("965510000000000000000"));
+  await token0.approve(routerV2.address, BigNumber.from("91512193227770000000000"));
+
+  await routerV2.addLiquidity(
+    btcbToken.address,
+    token0.address,
+    BigNumber.from("965510000000000000000"),
+    BigNumber.from("91512193227770000000000"),
+    "0",
+    "0",
+    deployerAddress,
+    FOREVER
+  );
+
   // PREPARE TEST CLASS
   const PriceHelper = (await ethers.getContractFactory("PriceHelper", deployer)) as PriceHelper__factory;
   priceHelper = (await upgrades.deployProxy(PriceHelper, [chainLinkOracle.address, usdToken.address])) as PriceHelper;
   await priceHelper.deployed();
+
+  casesData = [
+    //fairPrice= 2* sqrt(r0 * r1) * sqrt(p0 * p1) / totalSupply
+    //  (bnb-busd)
+    {
+      //fairPrice = 2* 9653542093654893404247640.49294092286780056392 * 20684293558156633356.07295450258677195686  / 9653542093654893404247640
+      //fairPrice = 41368587116313266712.14591111758671961799
+      token0Reserve: BigNumber.from("466712574325720000000000"),
+      token1Reserve: BigNumber.from("199675089295813000000000000"),
+      timestamp: "1642761186",
+      totalSupply: BigNumber.from("9653542093654893404247640"),
+      p0: BigNumber.from("427840000000000000000"),
+      p1: BigNumber.from("1000000000000000000"),
+      totalUSD: BigNumber.from("199675089295813000000000000").mul(2),
+      lpAddress: lpV2Token0Stable0.address,
+      fairPrice: BigNumber.from("41368587116313266712"),
+      hasUSD: true,
+    },
+    //  (busd-usdt)
+    {
+      token0Reserve: BigNumber.from("100000000000000000000"),
+      token1Reserve: BigNumber.from("100000000000000000000"),
+      timestamp: "1642759546",
+      totalSupply: BigNumber.from("100000000000000000000"),
+      p0: BigNumber.from("1000000000000000000"),
+      p1: BigNumber.from("1000000000000000000"),
+      totalUSD: BigNumber.from("100000000000000000000").mul(2),
+      lpAddress: lpV2Stable0Stable1.address,
+      fairPrice: BigNumber.from("2000000000000000000"),
+      hasUSD: true,
+    },
+    // btcb-busd
+    {
+      // fairPrice= 2 * 186111977726651963954050.05901829180428826807 * 205671373354091259153.06497271109524120409 /186111977726651963954050
+      // fairPrice= 411342746708182518306.13007586380178190719
+      token0Reserve: BigNumber.from("904717988720000000000"),
+      token1Reserve: BigNumber.from("38285596932068700000000000"),
+      timestamp: "1642759546",
+      totalSupply: BigNumber.from("186111977726651963954050"),
+      p0: BigNumber.from("42300713817358000000000"),
+      p1: BigNumber.from("1000000000000000000"),
+      totalUSD: BigNumber.from("38285596932068700000000000").mul(2),
+      lpAddress: lpV2BtcbStable0.address,
+      fairPrice: BigNumber.from("411342746708182518306"),
+      hasUSD: true,
+    },
+    // btcb-bnb
+    {
+      //fairPrice= 2 * 9399783916843206492029.37500865629023998066 * 4254167062965257682100.43529399434936881258 /9399783916843206492029
+      //fairPrice= 8508334125930515364200.87058798869873762516
+      token0Reserve: BigNumber.from("965510000000000000000"),
+      token1Reserve: BigNumber.from("91512193227770000000000"),
+      timestamp: "1642761962",
+      totalSupply: BigNumber.from("9399783916843206492029"),
+      p0: BigNumber.from("42300713817358000000000"),
+      p1: BigNumber.from("427840000000000000000"),
+      totalUSD: ethers.constants.Zero, // do no need to use in this case
+      lpAddress: lpV2BtcbToken0.address,
+      fairPrice: BigNumber.from("8508334125930515364200"),
+      hasUSD: false,
+    },
+  ];
 }
 
 beforeEach(async () => {
   await waffle.loadFixture(fixture);
 });
 
-context("chainlink get price should work properly", async () => {
+/* context("chainlink get price should work properly", async () => {
   it("chainlink get price should work properly", async () => {
     const [priceT0UsdToken] = await chainLinkOracleAsDeployer.getPrice(token0.address, usdToken.address);
     // result should be (priceT0UsdToken * 1e18) / (10**decimals) = (468290000000000000000 * 1e18) / (10**18) = 468290000000000000000
@@ -198,72 +300,63 @@ context("chainlink get price should work properly", async () => {
     const priceT1T0 = priceT1UsdToken.mul(ethers.utils.parseEther("1")).div(priceT0UsdToken);
     expect(priceT1T0).to.eq(BigNumber.from("21354289008947447"));
   });
-});
+}); */
 
 context("when incorrect input", async () => {
   it("#lpToDollar", async () => {
-    await expect(priceHelper.lpToDollar(BigNumber.from("0"), lpV2Token0Stable0.address)).to.be.revertedWith(
+    await expect(priceHelper.lpToDollar(ethers.constants.Zero, lpV2Token0Stable0.address)).to.be.revertedWith(
       "InvalidLPAmount()"
     );
 
-    await expect(priceHelper.lpToDollar("1", ADDRESS0)).to.be.revertedWith("InvalidLPAddress()");
+    await expect(priceHelper.lpToDollar(ethers.constants.One, ethers.constants.AddressZero)).to.be.revertedWith(
+      "InvalidLPAddress()"
+    );
   });
 
   it("#dollarToLP", async () => {
-    await expect(priceHelper.dollarToLP(BigNumber.from("0"), lpV2Token0Stable0.address)).to.be.revertedWith(
+    await expect(priceHelper.dollarToLP(ethers.constants.Zero, lpV2Token0Stable0.address)).to.be.revertedWith(
       "InvalidDollarAmount()"
     );
-    await expect(priceHelper.dollarToLP("1", ADDRESS0)).to.be.revertedWith("InvalidLPAddress()");
-  });
-});
-
-context("lpToDollar-success", async () => {
-  it("token-to-stable", async () => {
-    // totalSupply = 8869932693987378670153083
-    // r0 = 409514710336210000000000
-    // r1 = 192119364726297000000000000
-    // p0 = 468290000000000000000
-    // p1 =   1000000000000000000
-    // fairPrice = 2* sqrt(r0 * r1) * sqrt(p0 * p1) / totalSupply
-    // fairPrice      = 2* sqrt(409514710336210000000000 * 192119364726297000000000000) * sqrt(468290000000000000000 * 1000000000000000000) / 8869932693987378670153083
-    //                = 2 * (8869932693987378670153083.52417769176678540559) * (21640009242142203854.14703646049084548679)/8869932693987378670153083
-    //                = 2 * 21640009242142203854.14703646049084548679
-    // fairPrice(1LP) = 43280018484284407708.29407292098169097358
-    expect(await priceHelper.lpToDollar(ethers.utils.parseEther("1"), lpV2Token0Stable0.address)).to.be.eq(
-      "43280018484284407708"
-    );
-    expect(await priceHelper.lpToDollar(ethers.utils.parseEther("1000000"), lpV2Token0Stable0.address)).to.be.eq(
-      "43280018484284407708000000"
-    );
-  });
-
-  it("stable-to-stable", async () => {
-    // totalSupply = 100
-    // r0 = 100
-    // r1 = 100
-    // p0 = 1000000000000000000
-    // p1 = 1000000000000000000
-    expect(await priceHelper.lpToDollar(ethers.utils.parseEther("1"), lpV2Stable0Stable1.address)).to.be.eq(
-      "2000000000000000000"
-    );
-    expect(await priceHelper.lpToDollar(ethers.utils.parseEther("100"), lpV2Stable0Stable1.address)).to.be.eq(
-      "200000000000000000000"
+    await expect(priceHelper.dollarToLP(ethers.constants.One, ethers.constants.AddressZero)).to.be.revertedWith(
+      "InvalidLPAddress()"
     );
   });
 });
 
-context("dollarToLp-success", async () => {
+context("convert LP to dollar", async () => {
+  it("fairPrice", async () => {
+    for (const _case of casesData) {
+      const fairPrice = _case.fairPrice as BigNumber;
+      expect(await priceHelper.lpToDollar(ethers.utils.parseEther("1"), _case.lpAddress)).to.be.eq(fairPrice);
+
+      expect(await priceHelper.lpToDollar(ethers.utils.parseEther("2.5"), _case.lpAddress)).to.be.eq(
+        fairPrice.mul(ethers.utils.parseEther("2.5")).div(ethers.constants.WeiPerEther)
+      );
+      expect(await priceHelper.lpToDollar(ethers.utils.parseEther("9999999.123123"), _case.lpAddress)).to.be.eq(
+        fairPrice.mul(ethers.utils.parseEther("9999999.123123")).div(ethers.constants.WeiPerEther)
+      );
+    }
+  });
+  it("when compare fairPrice with normalLP formula", async () => {
+    for (const _case of casesData) {
+      // case hasUSd -> totalusd * 2
+      // otherwise, ((r0*p0) + (r1*p1))/totalSupply
+      const expectedValue = _case.hasUSD
+        ? _case.totalUSD.mul(ethers.constants.WeiPerEther).div(_case.totalSupply)
+        : _case.token0Reserve.mul(_case.p0).add(_case.token1Reserve.mul(_case.p1)).div(_case.totalSupply);
+      console.log("expectedValue", expectedValue);
+
+      assertBigNumberClosePercent(
+        expectedValue,
+        await priceHelper.lpToDollar(ethers.utils.parseEther("1"), _case.lpAddress),
+        "0.03"
+      );
+    }
+  });
+});
+
+/* context("dollarToLp-success", async () => {
   it("token-to-stable", async () => {
-    // totalSupply = 8869932693987378670153083
-    // r0 = 409514710336210000000000
-    // r1 = 192119364726297000000000000
-    // p0 = 468290000000000000000
-    // p1 =   1000000000000000000
-    // fairPrice = 2* sqrt(r0 * r1) * sqrt(p0 * p1) / totalSupply
-    // fairPrice      = 2* sqrt(409514710336210000000000 * 192119364726297000000000000) * sqrt(468290000000000000000 * 1000000000000000000) / 8869932693987378670153083
-    //                = 2 * (8869932693987378670153083.52417769176678540559) * (21640009242142203854.14703646049084548679)/8869932693987378670153083
-    //                = 2 * 21640009242142203854.14703646049084548679
-    // fairPrice(1LP) = 43280018484284407708.29407292098169097358
     expect(await priceHelper.dollarToLP(BigNumber.from("43280018484284407708"), lpV2Token0Stable0.address)).to.be.eq(
       ethers.utils.parseEther("1")
     );
@@ -291,3 +384,4 @@ context("dollarToLp-success", async () => {
     );
   });
 });
+ */
