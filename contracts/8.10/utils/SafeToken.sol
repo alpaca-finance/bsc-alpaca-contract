4799455d619175ddf88b5d69492101468d322c43
alpanaca <rblue4594@gmail.com>
2022-01-28 22:18:17 +0700

    fix: add contract check on contract low-level call

--- contracts/8.10/utils/SafeToken.sol
+++ contracts/8.10/utils/SafeToken.sol
@@ -29,6 +29,7 @@ library SafeToken {
   ) internal {
     // bytes4(keccak256(bytes('transfer(address,uint256)')));
     // solhint-disable-next-line avoid-low-level-calls
+    require(token.code.length > 0, "!not contract");
     (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
     require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeTransfer");
   }
@@ -41,6 +42,7 @@ library SafeToken {
   ) internal {
     // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
     // solhint-disable-next-line avoid-low-level-calls
+    require(token.code.length > 0, "!not contract");
     (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
     require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeTransferFrom");
   }
@@ -51,12 +53,14 @@ library SafeToken {
     uint256 value
   ) internal {
     // bytes4(keccak256(bytes('approve(address,uint256)')));
+    require(token.code.length > 0, "!not contract");
     (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
     require(success && (data.length == 0 || abi.decode(data, (bool))), "!safeApprove");
   }
 
   function safeTransferETH(address to, uint256 value) internal {
     // solhint-disable-next-line no-call-value
+    require(to.code.length > 0, "!not contract");
     (bool success, ) = to.call{ value: value }(new bytes(0));
     require(success, "!safeTransferETH");
   }
