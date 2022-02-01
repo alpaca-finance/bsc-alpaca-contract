b1d959d4ebb69c245a9056a62b09e4048e5f84cb
sora-alpaca <sora.alpaca@gmail.com>
2022-02-01 14:33:12 +0700

    [test] fix test description

--- test/integrations/protocol/delta-neutral/DeltaNeutralVault.test.ts
+++ test/integrations/protocol/delta-neutral/DeltaNeutralVault.test.ts
@@ -838,7 +838,7 @@ describe("DeltaNeutralVault", () => {
           console.log("aliceShare", aliceShare);
         });
 
-        context("when received shares greater than minimum shares should user receive", async () => {
+        context("when received shares is lower than minimum shares should user receive", async () => {
           it("should revert", async () => {
             const stableTokenAmount = ethers.utils.parseEther("500");
             const assetTokenAmount = ethers.utils.parseEther("500");
@@ -984,7 +984,7 @@ describe("DeltaNeutralVault", () => {
         });
 
         describe("_doWork", async () => {
-          context("alice try open position with another position id", async () => {
+          context("alice try open position with different position id", async () => {
             it("should revert", async () => {
               const stableTokenAmount = ethers.utils.parseEther("500");
               const assetTokenAmount = ethers.utils.parseEther("500");
@@ -1054,67 +1054,70 @@ describe("DeltaNeutralVault", () => {
         });
 
         describe("_depositHealthCheck", async () => {
-          context("when alice deposit with unsafe position equity on stable side", async () => {
-            it("should revert", async () => {
-              const stableTokenAmount = ethers.utils.parseEther("510");
-              const assetTokenAmount = ethers.utils.parseEther("500");
-              await baseTokenAsAlice.approve(deltaVault.address, stableTokenAmount);
-              const stableWorkbyteInput: IDepositWorkByte = {
-                posId: 1,
-                vaultAddress: stableVault.address,
-                workerAddress: stableVaultWorker.address,
-                twoSidesStrat: stableTwoSidesStrat.address,
-                principalAmount: ethers.utils.parseEther("0.13"),
-                borrowAmount: ethers.utils.parseEther("500"),
-                farmingTokenAmount: ethers.utils.parseEther("125"),
-                maxReturn: BigNumber.from(0),
-                minLpReceive: BigNumber.from(0),
-              };
-              const assetWorkbyteInput: IDepositWorkByte = {
-                posId: 1,
-                vaultAddress: assetVault.address,
-                workerAddress: assetVaultWorker.address,
-                twoSidesStrat: assetTwoSidesStrat.address,
-                principalAmount: ethers.utils.parseEther("375"),
-                borrowAmount: ethers.utils.parseEther("1500"),
-                farmingTokenAmount: ethers.utils.parseEther("375"),
-                maxReturn: BigNumber.from(0),
-                minLpReceive: BigNumber.from(0),
-              };
-              const stableWorkByte = buildDepositWorkByte(stableWorkbyteInput);
-              const assetWorkByte = buildDepositWorkByte(assetWorkbyteInput);
-              const data = ethers.utils.defaultAbiCoder.encode(
-                ["uint8[]", "uint256[]", "bytes[]"],
-                [
-                  [ACTION_WORK, ACTION_WORK],
-                  [0, 0],
-                  [stableWorkByte, assetWorkByte],
-                ]
-              );
-              let stableTokenPrice = ethers.utils.parseEther("1");
-              let assetTokenPrice = ethers.utils.parseEther("1");
-              let lpPrice = ethers.utils.parseEther("2");
-              mockPriceHelper.smocked.getTokenPrice.will.return.with((token: string) => {
-                if (token === baseToken.address) {
-                  return stableTokenPrice;
-                }
-                if (token === wbnb.address) {
-                  return assetTokenPrice;
-                }
-                return 0;
+          context(
+            "when alice deposit with actions that resulted in unsafe position equity on stable side",
+            async () => {
+              it("should revert", async () => {
+                const stableTokenAmount = ethers.utils.parseEther("510");
+                const assetTokenAmount = ethers.utils.parseEther("500");
+                await baseTokenAsAlice.approve(deltaVault.address, stableTokenAmount);
+                const stableWorkbyteInput: IDepositWorkByte = {
+                  posId: 1,
+                  vaultAddress: stableVault.address,
+                  workerAddress: stableVaultWorker.address,
+                  twoSidesStrat: stableTwoSidesStrat.address,
+                  principalAmount: ethers.utils.parseEther("0.13"),
+                  borrowAmount: ethers.utils.parseEther("500"),
+                  farmingTokenAmount: ethers.utils.parseEther("125"),
+                  maxReturn: BigNumber.from(0),
+                  minLpReceive: BigNumber.from(0),
+                };
+                const assetWorkbyteInput: IDepositWorkByte = {
+                  posId: 1,
+                  vaultAddress: assetVault.address,
+                  workerAddress: assetVaultWorker.address,
+                  twoSidesStrat: assetTwoSidesStrat.address,
+                  principalAmount: ethers.utils.parseEther("375"),
+                  borrowAmount: ethers.utils.parseEther("1500"),
+                  farmingTokenAmount: ethers.utils.parseEther("375"),
+                  maxReturn: BigNumber.from(0),
+                  minLpReceive: BigNumber.from(0),
+                };
+                const stableWorkByte = buildDepositWorkByte(stableWorkbyteInput);
+                const assetWorkByte = buildDepositWorkByte(assetWorkbyteInput);
+                const data = ethers.utils.defaultAbiCoder.encode(
+                  ["uint8[]", "uint256[]", "bytes[]"],
+                  [
+                    [ACTION_WORK, ACTION_WORK],
+                    [0, 0],
+                    [stableWorkByte, assetWorkByte],
+                  ]
+                );
+                let stableTokenPrice = ethers.utils.parseEther("1");
+                let assetTokenPrice = ethers.utils.parseEther("1");
+                let lpPrice = ethers.utils.parseEther("2");
+                mockPriceHelper.smocked.getTokenPrice.will.return.with((token: string) => {
+                  if (token === baseToken.address) {
+                    return stableTokenPrice;
+                  }
+                  if (token === wbnb.address) {
+                    return assetTokenPrice;
+                  }
+                  return 0;
+                });
+                mockPriceHelper.smocked.lpToDollar.will.return.with((lpAmount: BigNumber, lpToken: string) => {
+                  return lpAmount.mul(lpPrice).div(ethers.utils.parseEther("1"));
+                });
+                await expect(
+                  deltaVaultAsAlice.deposit(aliceAddress, 0, stableTokenAmount, assetTokenAmount, data, {
+                    value: assetTokenAmount,
+                  })
+                ).to.be.revertedWith("UnsafePositionEquity()");
               });
-              mockPriceHelper.smocked.lpToDollar.will.return.with((lpAmount: BigNumber, lpToken: string) => {
-                return lpAmount.mul(lpPrice).div(ethers.utils.parseEther("1"));
-              });
-              await expect(
-                deltaVaultAsAlice.deposit(aliceAddress, 0, stableTokenAmount, assetTokenAmount, data, {
-                  value: assetTokenAmount,
-                })
-              ).to.be.revertedWith("UnsafePositionEquity()");
-            });
-          });
+            }
+          );
 
-          context("when alice deposit with unsafe position equity on asset side", async () => {
+          context("when alice deposit with actions that resulted in unsafe position equity on asset side", async () => {
             it("should revert", async () => {
               const stableTokenAmount = ethers.utils.parseEther("500");
               const assetTokenAmount = ethers.utils.parseEther("550");
@@ -1174,7 +1177,7 @@ describe("DeltaNeutralVault", () => {
             });
           });
 
-          context("when alice deposit with unsafe debt value on stable side", async () => {
+          context("when alice deposit with actions that resulted in unsafe debt value on stable side", async () => {
             it("should revert", async () => {
               const stableTokenAmount = ethers.utils.parseEther("500");
               const assetTokenAmount = ethers.utils.parseEther("500");
@@ -1234,7 +1237,7 @@ describe("DeltaNeutralVault", () => {
             });
           });
 
-          context("when alice deposit with unsafe debt value on asset side", async () => {
+          context("when alice deposit with actions that resulted in unsafe debt value on asset side", async () => {
             it("should revert", async () => {
               const stableTokenAmount = ethers.utils.parseEther("500");
               const assetTokenAmount = ethers.utils.parseEther("500");
@@ -1296,7 +1299,7 @@ describe("DeltaNeutralVault", () => {
         });
 
         describe("_outstandingCheck", () => {
-          context("when stable token amount has descresed", async () => {
+          context("when stable token amount in contract has descresed after deposit", async () => {
             it("should revert", async () => {
               await baseTokenAsAlice.transfer(deltaVault.address, ethers.utils.parseEther("400"));
               const reduceAmount = ethers.utils.parseEther("10");
@@ -1371,7 +1374,7 @@ describe("DeltaNeutralVault", () => {
               );
             });
 
-            context("when asset token amount has descresed", async () => {
+            context("when asset token amount in the contract has descresed after deposit", async () => {
               it("should revert", async () => {
                 await wbnbTokenAsAlice.transfer(deltaVault.address, ethers.utils.parseEther("400"));
                 const reduceAmount = ethers.utils.parseEther("10");
