b62c4e1f7c28ef6a7dfa89d20cd8ba2979c4cd10
jr-alpaca <uviveramrom@gmail.com>
2022-02-08 13:18:32 +0700

    feat: check withdraw value with share value

--- contracts/8.10/protocol/DeltaNeutralVault.sol
+++ contracts/8.10/protocol/DeltaNeutralVault.sol
@@ -65,9 +65,11 @@ contract DeltaNeutralVault is ERC20Upgradeable, ReentrancyGuardUpgradeable, Owna
   error PositionsIsHealthy();
   error InsufficientTokenReceived(address _token, uint256 _requiredAmount, uint256 _receivedAmount);
   error InsufficientShareReceived(uint256 _requiredAmount, uint256 _receivedAmount);
+  error InsufficientShareWithdraw();
   error UnTrustedPrice();
   error BountyExceedLimit();
   error PositionValueExceedLimit();
+  error WithdrawValueExceedShareValue(uint256 _withdrawValue, uint256 _shareValue);
 
   struct Outstanding {
     uint256 stableAmount;
@@ -314,6 +316,7 @@ contract DeltaNeutralVault is ERC20Upgradeable, ReentrancyGuardUpgradeable, Owna
 
     uint256 _withdrawalFeeBps = config.feeExemptedCallers(msg.sender) ? 0 : config.withdrawalFeeBps();
     uint256 _shareToWithdraw = ((MAX_BPS - _withdrawalFeeBps) * _shareAmount) / MAX_BPS;
+    uint256 _withdrawShareValue = shareToValue(_shareAmount);
     // burn shares from share owner
     _burn(msg.sender, _shareAmount);
     // mint shares equal to withdrawal fee to treasury.
@@ -354,6 +357,9 @@ contract DeltaNeutralVault is ERC20Upgradeable, ReentrancyGuardUpgradeable, Owna
     }
 
     // sanity check
+    if (_withdrawShareValue < _withdrawValue) {
+      revert WithdrawValueExceedShareValue(_withdrawValue, _withdrawShareValue);
+    }
     _withdrawHealthCheck(_withdrawValue, _positionInfoBefore, _positionInfoAfter);
     _outstandingCheck(_outstandingBefore, _outstandingAfter);
 
