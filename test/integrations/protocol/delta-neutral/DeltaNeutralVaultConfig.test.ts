56875e2a9643ec501b4de47e383ffaa18756ede4
jr-alpaca <uviveramrom@gmail.com>
2022-02-15 21:27:05 +0700

    fix: check _newWithdrawalFee when setFees

--- test/integrations/protocol/delta-neutral/DeltaNeutralVaultConfig.test.ts
+++ test/integrations/protocol/delta-neutral/DeltaNeutralVaultConfig.test.ts
@@ -308,4 +308,28 @@ describe("DeltaNeutralVaultConfig", () => {
       });
     });
   });
+
+  describe("#setFees", async () => {
+    context("when not owner call setFees", async () => {
+      it("should revert", async () => {
+        await expect(deltaNeutralVaultConfigAsAlice.setFees(0, 0, 0)).to.be.reverted;
+      });
+    });
+    context("when set too much fee", async () => {
+      it("should revert", async () => {
+        await expect(deltaNeutralVaultConfig.setFees(1001, 1002, 1003)).to.be.revertedWith(
+          "TooMuchFee(1001, 1002, 1003)"
+        );
+      });
+    });
+    context("when owner call setFees", async () => {
+      it("should be able to set fee", async () => {
+        const setFeesTx = await deltaNeutralVaultConfig.setFees(500, 500, 500);
+        expect(await deltaNeutralVaultConfig.depositFeeBps()).to.eq(500);
+        expect(await deltaNeutralVaultConfig.withdrawalFeeBps()).to.eq(500);
+        expect(await deltaNeutralVaultConfig.mangementFeeBps()).to.eq(500);
+        expect(setFeesTx).to.emit(deltaNeutralVaultConfig, "LogSetFees").withArgs(deployerAddress, 500, 500, 500);
+      });
+    });
+  });
 });
