0554aee41d1d98393b58370a97ab0c4a71b30106
sora-alpaca <sora.alpaca@gmail.com>
2022-02-22 13:25:34 +0700

    [chore] prevent invalid parameter

--- contracts/8.10/protocol/DeltaNeutralVault.sol
+++ contracts/8.10/protocol/DeltaNeutralVault.sol
@@ -72,6 +72,7 @@ contract DeltaNeutralVault is ERC20Upgradeable, ReentrancyGuardUpgradeable, Owna
   error DeltaNeutralVault_IncorrectNativeAmountDeposit();
   error DeltaNeutralVault_InvalidLpToken();
   error DeltaNeutralVault_InvalidInitializedAddress();
+  error DeltaNeutralVault_InvalidShareAmount();
 
   struct Outstanding {
     uint256 stableAmount;
@@ -327,14 +328,18 @@ contract DeltaNeutralVault is ERC20Upgradeable, ReentrancyGuardUpgradeable, Owna
     uint256 _minAssetTokenAmount,
     bytes calldata _data
   ) external onlyEOAorWhitelisted collectFee nonReentrant returns (uint256) {
+    if (_shareAmount == 0) revert DeltaNeutralVault_InvalidShareAmount();
+
     PositionInfo memory _positionInfoBefore = positionInfo();
     Outstanding memory _outstandingBefore = _outstanding();
 
     uint256 _withdrawalFeeBps = config.feeExemptedCallers(msg.sender) ? 0 : config.withdrawalFeeBps();
     uint256 _shareToWithdraw = ((MAX_BPS - _withdrawalFeeBps) * _shareAmount) / MAX_BPS;
     uint256 _withdrawShareValue = shareToValue(_shareToWithdraw);
+
     // burn shares from share owner
     _burn(msg.sender, _shareAmount);
+
     // mint shares equal to withdrawal fee to treasury.
     _mint(config.getTreasuryAddr(), _shareAmount - _shareToWithdraw);
 
