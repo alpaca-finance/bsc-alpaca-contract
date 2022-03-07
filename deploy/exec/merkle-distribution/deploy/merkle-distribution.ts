1d09b137ff505a78cbcd150c72f56abe26b83f64
spicysquid168 <spicysquid168@gmail.com>
2022-03-07 13:56:26 +0700

    deploy: merkle distribution aip1 to testnet

--- deploy/exec/merkle-distribution/deploy/merkle-distribution.ts
+++ deploy/exec/merkle-distribution/deploy/merkle-distribution.ts
@@ -1,10 +1,10 @@
-import { ethers } from 'hardhat';
-import { MerkleDistributor__factory } from "../../../../typechain"
-import { DeployFunction } from 'hardhat-deploy/types';
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { ethers } from "hardhat";
+import { MerkleDistributor__factory } from "../../../../typechain";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
-    /*
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
     ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
     ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
     ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
@@ -13,33 +13,21 @@ const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
     ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
     Check all variables below before execute the deployment script
     */
-    const MERKLE_ROOT = '0x6448eaef682da62f86be2b8c5dbb098b1d2f40cc9b1615b2302c0ef1e619ffda'; //merkle root ITAM week 1 testnet.
-    const FEATURE_TOKEN_ADDRESS = '0x04c747b40be4d535fc83d09939fb0f626f32800b'; // itam token address testnet
+  const MERKLE_ROOT = "0xfa187531f35e9281cf5832abceb976b8cb641f0eb3e59a9053039625507dae1d";
+  const FEATURE_TOKEN_ADDRESS = "0x354b3a11D5Ea2DA89405173977E271F58bE2897D";
 
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    
-    console.log(">> Deploying a Merkle distributor contract");
-    const MerkleDistributorContract = (await ethers.getContractFactory(
-        "MerkleDistributor",
-        (await ethers.getSigners())[0]
-    )) as MerkleDistributor__factory;
-    const merkleDistributor = await MerkleDistributorContract.deploy(FEATURE_TOKEN_ADDRESS, MERKLE_ROOT);
-    await merkleDistributor.deployed();
-    console.log(`>> Deployed at ${merkleDistributor.address}`);
-    console.log("✅ Done");
+  console.log(">> Deploying a Merkle distributor contract");
+  const MerkleDistributorContract = (await ethers.getContractFactory(
+    "MerkleDistributor",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as MerkleDistributor__factory;
+  const merkleDistributor = await MerkleDistributorContract.deploy(FEATURE_TOKEN_ADDRESS, MERKLE_ROOT);
+  await merkleDistributor.deployed();
+  console.log(`>> Deployed at ${merkleDistributor.address}`);
+  console.log("✅ Done");
 };
 
 export default func;
-func.tags = ['MerkleDistributor']
+func.tags = ["MerkleDistributor"];
