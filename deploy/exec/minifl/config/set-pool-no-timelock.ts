de76f52ac7b6100908e97d2d2a3b5fdc93cc815c
black-alpacaa <black.alpacaa@gmail.com>
2022-03-18 15:14:30 +0700

    feat: add deploy scripts add-pool-no-timelock, set-pool-no-timelock, set-max-reinvest-bouty-workers03.

---
+++ deploy/exec/minifl/config/set-pool-no-timelock.ts
@@ -0,0 +1,76 @@
+import { MiniFL__factory } from "./../../../../typechain/factories/MiniFL__factory";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, network } from "hardhat";
+import { Timelock__factory } from "../../../../typechain";
+import { getConfig } from "../../../entities/config";
+
+interface IUpdate {
+  STAKING_TOKEN: string;
+  ALLOC_POINT: number;
+  OVERWRITE: boolean;
+  WITH_UPDATE: boolean;
+}
+
+interface IInput {
+  pId: number;
+  stakingPool: string;
+  allocPoint: number;
+  rewarder: string;
+  overwrite: boolean;
+  withUpdate: boolean;
+}
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const UPDATES: Array<IUpdate> = [
+    {
+      STAKING_TOKEN: "fdAlpacaToken",
+      ALLOC_POINT: 225,
+      OVERWRITE: false,
+      WITH_UPDATE: true,
+    },
+  ];
+
+  const config = getConfig();
+  const deployer = (await ethers.getSigners())[0];
+  const inputs: Array<IInput> = [];
+  const miniFL = MiniFL__factory.connect(config.MiniFL!.address, deployer);
+  let nonce = await deployer.getTransactionCount();
+
+  /// @dev derived input
+  for (let i = 0; i < UPDATES.length; i++) {
+    const pool = config.MiniFL!.pools.find((p) => p.stakingToken == UPDATES[i].STAKING_TOKEN);
+    if (pool !== undefined) {
+      inputs.push({
+        pId: pool.id,
+        stakingPool: pool.stakingToken,
+        allocPoint: UPDATES[i].ALLOC_POINT,
+        rewarder: pool.rewarder,
+        overwrite: UPDATES[i].OVERWRITE,
+        withUpdate: UPDATES[i].WITH_UPDATE,
+      });
+    } else {
+      throw new Error(`not found ${UPDATES[i].STAKING_TOKEN}`);
+    }
+  }
+
+  for (const input of inputs) {
+    console.log(`>> >> MiniFL: set ${input.stakingPool} with ${input.allocPoint}`);
+    await miniFL.setPool(input.pId, input.allocPoint, input.rewarder, input.overwrite, input.withUpdate, {
+      nonce: nonce++,
+      gasPrice: ethers.utils.parseUnits("10", "gwei"),
+    });
+  }
+};
+
+export default func;
+func.tags = ["MiniFLSetPool"];
---
+++ deploy/exec/workers/config/set-max-reinvest-bouty-bps-workers03.ts
@@ -0,0 +1,96 @@
+import { SpookyWorker03__factory } from "./../../../../typechain/factories/SpookyWorker03__factory";
+import { TombWorker03__factory } from "./../../../../typechain/factories/TombWorker03__factory";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+import { ConfigEntity } from "../../../entities";
+
+interface IWorker {
+  WORKER_NAME: string;
+  ADDRESS: string;
+}
+
+type IWorkers = Array<IWorker>;
+
+/**
+ * @description Deployment script for setting workers' beneficial vault related data
+ * @param  {HardhatRuntimeEnvironment} hre
+ */
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  /*
+  ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
+  ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
+  ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
+  ░░████╔═████║░██╔══██║██╔══██╗██║╚████║██║██║╚████║██║░░╚██╗
+  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██║░╚███║██║██║░╚███║╚██████╔╝
+  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚══╝░╚═════╝░
+  Check all variables below before execute the deployment script
+  */
+  const workerInputs: Array<string> = [
+    "WFTM-ALPACA SpookyWorker",
+    "TUSD-USDC SpookyWorker",
+    "WFTM-USDC SpookyWorker",
+    "MIM-WFTM SpookyWorker",
+    "DAI-WFTM SpookyWorker",
+    "fUSDT-WFTM SpookyWorker",
+    "BTC-WFTM SpookyWorker",
+    "ALPACA-WFTM SpookyWorker",
+    "ETH-WFTM SpookyWorker",
+    "BOO-WFTM SpookyWorker",
+    "USDC-WFTM SpookyWorker",
+    "WFTM-TOMB TombWorker",
+    "TSHARE-WFTM TombWorker",
+    "TOMB-WFTM TombWorker",
+  ];
+  const MAX_REINVEST_BOUNTY_BPS = "900";
+
+  const config = ConfigEntity.getConfig();
+  const allWorkers: IWorkers = config.Vaults.reduce((accum, vault) => {
+    return accum.concat(
+      vault.workers.map((worker) => {
+        return {
+          WORKER_NAME: worker.name,
+          ADDRESS: worker.address,
+        };
+      })
+    );
+  }, [] as IWorkers);
+
+  const TO_BE_UPDATED_WORKERS: IWorkers = workerInputs.map((workerInput) => {
+    // 1. find each worker having an identical name as workerInput
+    // 2. if hit return
+    // 3. other wise throw error
+    const hit = allWorkers.find((worker) => {
+      return worker.WORKER_NAME === workerInput;
+    });
+
+    if (!!hit) return hit;
+
+    throw new Error(`could not find ${workerInput}`);
+  });
+
+  const deployer = (await ethers.getSigners())[0];
+
+  let nonce = await deployer.getTransactionCount();
+
+  for (let i = 0; i < TO_BE_UPDATED_WORKERS.length; i++) {
+    console.log(`>> Set max reinvest for ${TO_BE_UPDATED_WORKERS[i].WORKER_NAME}`);
+    if (TO_BE_UPDATED_WORKERS[i].WORKER_NAME.includes("TombWorker")) {
+      const tombWorker03 = TombWorker03__factory.connect(TO_BE_UPDATED_WORKERS[i].ADDRESS, deployer);
+      await tombWorker03.setMaxReinvestBountyBps(MAX_REINVEST_BOUNTY_BPS, {
+        nonce: nonce++,
+        gasPrice: ethers.utils.parseUnits("10", "gwei"),
+      });
+      continue;
+    }
+    const spookyWorker03 = SpookyWorker03__factory.connect(TO_BE_UPDATED_WORKERS[i].ADDRESS, deployer);
+    await spookyWorker03.setMaxReinvestBountyBps(MAX_REINVEST_BOUNTY_BPS, {
+      nonce: nonce++,
+      gasPrice: ethers.utils.parseUnits("10", "gwei"),
+    });
+    console.log("✅ Done");
+  }
+};
+
+export default func;
+func.tags = ["SetMaxReinvestBountyBpsWorkers03"];
