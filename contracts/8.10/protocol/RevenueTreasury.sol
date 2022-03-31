956324ebe31208e46468000c358b6ec16e4711d2
alpanaca <rblue4594@gmail.com>
2022-03-31 14:20:15 +0700

    chore: comment

--- contracts/8.10/protocol/RevenueTreasury.sol
+++ contracts/8.10/protocol/RevenueTreasury.sol
@@ -119,7 +119,7 @@ contract RevenueTreasury is Initializable, OwnableUpgradeable {
 
       if (vaultSwapPath.length != 0) {
         uint256[] memory expectedAmountsIn = router.getAmountsIn(remaining, vaultSwapPath);
-        // if the exepected amount in < _split, swap _split amount
+        // if the exepected amount in < _split, then swap with _split amount
         // otherwise, swap only neeeded
         uint256 _swapAmount = expectedAmountsIn[0] < _split ? expectedAmountsIn[0] : _split;
         token.safeApprove(address(router), _swapAmount);
