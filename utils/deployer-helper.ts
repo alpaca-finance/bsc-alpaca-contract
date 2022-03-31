dca9dc7b7aa49d3264326d6b5218e146482adf62
abc-alpaca <gabcdef637@gmail.com>
2022-03-31 18:31:47 +0700

    feat: fix getdeployer

--- utils/deployer-helper.ts
+++ utils/deployer-helper.ts
@@ -2,15 +2,16 @@ import { Provider } from "@ethersproject/providers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers, network } from "hardhat";
 import { JsonRpcProvider } from "@ethersproject/providers";
+import { HttpNetworkUserConfig } from "hardhat/types";
 
 export async function getDeployer(): Promise<SignerWithAddress> {
-  const defaultDeployer = (await ethers.getSigners())[0];
-
-  const provider = ethers.getDefaultProvider(process.env.FORK_RPC) as JsonRpcProvider;
-  const signer = provider.getSigner(process.env.DEPLOYER_ADDRESS);
-  const mainnetForkDeployer = await SignerWithAddress.create(signer);
+  const [defaultDeployer] = await ethers.getSigners();
 
   if (network.name === "mainnetfork" || network.name === "fantom_mainnetfork") {
+    const provider = ethers.getDefaultProvider((network.config as HttpNetworkUserConfig).url) as JsonRpcProvider;
+    const signer = provider.getSigner(defaultDeployer.address);
+    const mainnetForkDeployer = await SignerWithAddress.create(signer);
+
     return mainnetForkDeployer;
   }
 
