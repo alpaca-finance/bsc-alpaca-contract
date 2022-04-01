fd0f5fcbc2699f61192e978b368c95ec12863c15
abc-alpaca <gabcdef637@gmail.com>
2022-04-01 18:53:57 +0700

    chore: move div after mul

--- deploy/exec/delta-neutral-vault/config/init-positions.ts
+++ deploy/exec/delta-neutral-vault/config/init-positions.ts
@@ -187,9 +187,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const assetTokenConversionFactor = initPosition.assetDecimal - initPosition.stableDecimal;
     const borrowAmountAssetPosition = farmingTokenAssetPosition
       .mul(ethers.constants.WeiPerEther)
-      .div(assetPrice)
       .mul(borrowMultiplierPosition)
-      .mul(10 ** assetTokenConversionFactor);
+      .mul(10 ** assetTokenConversionFactor)
+      .div(assetPrice);
+
     console.log(`>> borrowAmountAssetPosition: ${borrowAmountAssetPosition}`);
 
     const stableWorkbyteInput: IDepositWorkByte = {
