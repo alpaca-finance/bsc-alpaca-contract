a2faa754cb159867aec3ea72f7b1c7be6c0fbfbc
spicysquid168 <spicysquid168@gmail.com>
2022-04-04 10:19:09 +0700

    chore: transfer usdc vault to timelock

--- deploy/exec/ownable/config/transfer-ownership.ts
+++ deploy/exec/ownable/config/transfer-ownership.ts
@@ -15,7 +15,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const TO_BE_LOCKED = ["0x10c3d6330272Fee8bf9d3E42D0b790052ebc09DF", "0xDBF13eB90b81014D192c7e9749d607d6DC533459"];
+  const TO_BE_LOCKED = ["0x800933D685E7Dc753758cEb77C8bd34aBF1E26d7", "0x01cf56544A303025C77c76cA77982cbd8655fCBC"];
 
   const config = ConfigEntity.getConfig();
   const deployer = (await ethers.getSigners())[0];
@@ -24,8 +24,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   for (let i = 0; i < TO_BE_LOCKED.length; i++) {
     console.log(`>> Transferring ownership of ${TO_BE_LOCKED[i]} to TIMELOCK`);
     const ownable = Ownable__factory.connect(TO_BE_LOCKED[i], deployer);
-    await ownable.transferOwnership(config.Timelock, { gasPrice: ethers.utils.parseUnits("20", "gwei"), nonce });
-    nonce++;
+    await ownable.transferOwnership(config.Timelock, {
+      gasPrice: ethers.utils.parseUnits("20", "gwei"),
+      nonce: nonce++,
+    });
     console.log("✅ Done");
   }
 };
