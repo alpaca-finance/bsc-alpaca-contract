f182cc6f24c33667c9cefa104fbef5036b8410d1
alpanaca <rblue4594@gmail.com>
2022-04-04 15:57:56 +0700

    feat: add Biswap LP interface

---
+++ contracts/6/protocol/interfaces/IBiswapLiquidityPair.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
+pragma solidity 0.6.6;
+
+import "./ISwapPairLike.sol";
+interface IBiswapLiquidityPair is ISwapPairLike {
+  function swapFee() external view returns (uint32);
+}
