9048903abc91d5a30abf24e8469f6bebfd501097
sora-alpaca <sora.alpaca@gmail.com>
2022-04-19 17:16:36 +0700

    chore: remove unused func

--- utils/address.ts
+++ utils/address.ts
@@ -1,9 +1,3 @@
-import ethers from "ethers";
-
 export function compare(address0: string, address1: string): boolean {
   return address0.toLowerCase() === address1.toLowerCase();
-}
-
-export function validateAddress(address: string): boolean {
-  return address !== "" && address !== ethers.constants.AddressZero;
 }
