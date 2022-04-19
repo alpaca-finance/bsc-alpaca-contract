5ca3dd3fc4eef15244f954aedecbd74dea317d1d
sora-alpaca <sora.alpaca@gmail.com>
2022-04-19 15:10:32 +0700

    chore: use ethers constants

--- utils/address.ts
+++ utils/address.ts
@@ -1,9 +1,9 @@
-import { zeroAddress } from "ethereumjs-util";
+import ethers from "ethers";
 
 export function compare(address0: string, address1: string): boolean {
   return address0.toLowerCase() === address1.toLowerCase();
 }
 
 export function validateAddress(address: string): boolean {
-  return address !== "" && address !== zeroAddress();
+  return address !== "" && address !== ethers.constants.AddressZero;
 }
