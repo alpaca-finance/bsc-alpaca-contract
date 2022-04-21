b25be7c43eae8b8a95ac9004d8ef8661cc3a63c1
sora-alpaca <sora.alpaca@gmail.com>
2022-04-21 18:02:10 +0700

    chore: remove unused function

--- deploy/services/file.ts
+++ deploy/services/file.ts
@@ -16,25 +16,3 @@ export function writeJson(fileName: string, content: any, timestampInput?: numbe
   }
   fs.writeFileSync(`./deploy/results/${timestamp}_${fileName}.json`, JSON.stringify(content, null, 2));
 }
-
-export function writeConfigJson(config: Config) {
-  switch (network.name) {
-    case "mainnet":
-    case "mainnetfork":
-      fs.writeFileSync(`.mainnet.json`, JSON.stringify(config, null, 2));
-      break;
-    case "testnet":
-      fs.writeFileSync(`.testnet.json`, JSON.stringify(config, null, 2));
-      break;
-    case "fantom_mainnet":
-      fs.writeFileSync(`.fantom_mainnet.json`, JSON.stringify(config, null, 2));
-      break;
-    case "fantom_testnet":
-    case "fantom_mainnetfork":
-      fs.writeFileSync(`.fantom_testnet.json`, JSON.stringify(config, null, 2));
-      break;
-    default:
-      fs.writeFileSync(`.default.json`, JSON.stringify(config, null, 2));
-      break;
-  }
-}
