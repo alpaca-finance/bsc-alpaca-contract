da39a899da55eb822da5b0b13365630c9411c7c4
alpanaca <rblue4594@gmail.com>
2022-04-29 19:07:06 +0700

    test: setValuePerxALPACA

--- solidity/tests/protocol/private-automated-vault/xALPACACreditor.t.sol
+++ solidity/tests/protocol/private-automated-vault/xALPACACreditor.t.sol
@@ -13,7 +13,7 @@ import { MockContract } from "../../utils/MockContract.sol";
 // solhint-disable contract-name-camelcase
 contract xAlpacaCreditor_Test is BaseTest {
   using mocking for *;
-  uint64 private constant VALUE_PER_XALPACA = 2e18;
+  uint64 private constant VALUE_PER_XALPACA = 2 ether;
 
   xALPACACreditorLike private _creditor;
   IxALPACA private _xALPACA;
@@ -23,12 +23,27 @@ contract xAlpacaCreditor_Test is BaseTest {
   function setUp() external {
     _xALPACA = IxALPACA(address(new MockContract()));
     _xALPACA.epoch.mockv(1);
-    _xALPACA.balanceOf.mockv(_userAddress, 1e18);
+    _xALPACA.balanceOf.mockv(_userAddress, 1 ether);
 
     _creditor = _setupxALPACACreditor(address(_xALPACA), VALUE_PER_XALPACA);
   }
 
   function testCorrectness_getUserCredit() external {
-    assertEq(_creditor.getUserCredit(_userAddress), 2e18);
+    assertEq(_creditor.getUserCredit(_userAddress), 2 ether);
+  }
+
+  function testCorrectness_afterUpdateValuePerxALPACA() external {
+    _creditor.setValuePerxALPACA(4 ether);
+    assertEq(_creditor.getUserCredit(_userAddress), 4 ether);
+  }
+
+  function testCannotSetValuePerXalpacaMoreThanThreshold() external {
+    vm.expectRevert(abi.encodeWithSignature("xALPACACreditor_ValueTooHigh()"));
+    _creditor.setValuePerxALPACA(10000e18);
+  }
+
+  function testCanSetValuePerXalpacaLessThanThreshold(uint256 _value) external {
+    vm.assume(_value < 1000 ether);
+    _creditor.setValuePerxALPACA(_value);
   }
 }
