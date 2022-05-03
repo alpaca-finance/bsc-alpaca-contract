27d1f6dd3a570a62c4e650ebde9db08eb7d7eb46
alpanaca <rblue4594@gmail.com>
2022-05-03 15:38:28 +0700

    chore: remove unused import

--- solidity/tests/protocol/private-automated-vault/xALPACACreditor.t.sol
+++ solidity/tests/protocol/private-automated-vault/xALPACACreditor.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4 <0.9.0;
 
-import { BaseTest, console, xALPACACreditorLike, console } from "../../base/BaseTest.sol";
+import { BaseTest, xALPACACreditorLike } from "../../base/BaseTest.sol";
 
 import { xALPACACreditor } from "../../../contracts/8.13/xALPACACreditor.sol";
 import { IxALPACA } from "../../../contracts/8.13/interfaces/IxALPACA.sol";
