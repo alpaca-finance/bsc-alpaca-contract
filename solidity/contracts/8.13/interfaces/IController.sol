90103a2a4131a7c2f6945341cf2325bc1fdb85a9
alpanaca <rblue4594@gmail.com>
2022-05-19 13:07:41 +0700

    feat: add controller to delta vault config

---
+++ solidity/contracts/8.13/interfaces/IController.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: BUSL
+/**
+  ∩~~~~∩ 
+  ξ ･×･ ξ 
+  ξ　~　ξ 
+  ξ　　 ξ 
+  ξ　　 “~～~～〇 
+  ξ　　　　　　 ξ 
+  ξ ξ ξ~～~ξ ξ ξ 
+　 ξ_ξξ_ξ　ξ_ξξ_ξ
+Alpaca Fin Corporation
+*/
+
+pragma solidity 0.8.13;
+
+interface IController {
+  function totalCredit(address _user) external view returns (uint256);
+
+  function usedCredit(address _user) external view returns (uint256);
+
+  function availableCredit(address _user) external view returns (uint256);
+}
