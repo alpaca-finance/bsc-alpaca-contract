2eb2bad600372496869f23f1d13abbbaeb4e8597
abc-alpaca <gabcdef637@gmail.com>
2022-05-25 11:08:19 +0700

    chore: rename

--- deploy/exec/delta-neutral-vault-config/config/set-controller.ts
+++ deploy/exec/delta-neutral-vault-config/config/set-controller.ts
@@ -16,14 +16,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const DLTA_VAULT_SYMBOL = ["n8x-BNBUSDT-PCS2"];
+  const DELTA_VAULT_SYMBOL = ["n8x-BNBUSDT-PCS2"];
 
   const deployer = await getDeployer();
   const config = getConfig();
   const CONTROLLER = config.AutomatedVaultController!.address;
 
   // VALIDATING ALL DLTA_VAULT_SYMBOL
-  const configs = DLTA_VAULT_SYMBOL.map((inputVaultSymbol) => {
+  const configs = DELTA_VAULT_SYMBOL.map((inputVaultSymbol) => {
     const deltaVaultConfig = config.DeltaNeutralVaults.find((o) => compare(o.symbol, inputVaultSymbol));
     if (!deltaVaultConfig) {
       throw new Error(`ERROR : DLTA_VAULT_SYMBOL is INVALID : ${inputVaultSymbol}`);
