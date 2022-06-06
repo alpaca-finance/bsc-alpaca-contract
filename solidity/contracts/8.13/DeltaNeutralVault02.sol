5574b78f1956dc78b8b5e7845cf4ad0e63b7cbcc
alpanaca <rblue4594@gmail.com>
2022-06-06 20:03:48 +0700

    fix: wrong cacl logic

--- solidity/contracts/8.13/DeltaNeutralVault02.sol
+++ solidity/contracts/8.13/DeltaNeutralVault02.sol
@@ -338,9 +338,6 @@ contract DeltaNeutralVault02 is ERC20Upgradeable, ReentrancyGuardUpgradeable, Ow
     // For private vault, deposit value should not exeed credit
     // Check availableCredit from msg.sender since user interact with contract directly
     IController _controller = IController(config.controller());
-    if (address(_controller) != address(0) && _depositValue > _controller.availableCredit(msg.sender)) {
-      revert DeltaNeutralVault_ExceedCredit();
-    }
 
     // Calculate share from the value gain against the total equity before execution of actions
     uint256 _sharesToUser = _valueToShare(
@@ -358,7 +355,12 @@ contract DeltaNeutralVault02 is ERC20Upgradeable, ReentrancyGuardUpgradeable, Ow
     _outstandingCheck(_outstandingBefore, _outstanding());
 
     // Deduct credit from msg.sender regardless of the _shareReceiver.
-    if (address(_controller) != address(0)) _controller.onDeposit(msg.sender, _sharesToUser);
+    if (address(_controller) != address(0)) {
+      _controller.onDeposit(msg.sender, _sharesToUser);
+      if (_controller.totalCredit(msg.sender) < _controller.usedCredit(msg.sender)) {
+        revert DeltaNeutralVault_ExceedCredit();
+      }
+    }
 
     emit LogDeposit(msg.sender, _shareReceiver, _sharesToUser, _stableTokenAmount, _assetTokenAmount);
     return _sharesToUser;
