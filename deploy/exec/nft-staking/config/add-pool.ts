87a9a41b196d2ce1f8481bf306e4181eedc5223a
sora-alpaca <sora.alpaca@gmail.com>
2022-06-09 15:05:27 +0700

    feat: NFTStaking add pool script

--- deploy/exec/nft-staking/config/add-pool.ts
+++ deploy/exec/nft-staking/config/add-pool.ts
@@ -1,9 +1,29 @@
-import { NFTStaking } from "../../../../typechain/NFTStaking";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { getDeployer } from "../../../../utils/deployer-helper";
-import { UpgradeableContractDeployer } from "../../../deployer";
 import { ConfigFileHelper } from "../../../helper";
+import { NFTStaking__factory } from "../../../../typechain";
+
+const Alpies = "0x57A7c5d10c3F87f5617Ac1C60DA60082E44D539e";
+const AlpiesWormhole = "0x077dc15c7ef8107e77daad8139158d9391261d40";
+
+interface AddPoolConfig {
+  address: string;
+  poolWeigth: number;
+  minPeriod: number;
+  maxPeriod: number;
+}
+
+const _buildInput = (address: string, poolWeigth: number, minPeriod: number, maxPeriod: number) => {
+  return {
+    address,
+    poolWeigth,
+    minPeriod,
+    maxPeriod,
+  };
+};
+
+const DAY_IN_SECONDS = 86400;
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
@@ -16,13 +36,25 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const deployer = await getDeployer();
-  const NftStaking = new UpgradeableContractDeployer<NFTStaking>(deployer, "NFTStaking");
+  const nftContractConfigsInput = [
+    _buildInput(Alpies, 1000, 0, DAY_IN_SECONDS * 356),
+    _buildInput(AlpiesWormhole, 1000, 0, DAY_IN_SECONDS * 356),
+  ];
 
-  const { contract: nftStaking } = await NftStaking.deploy([]);
-  const cfh = new ConfigFileHelper();
-  cfh.addAlpiesStaking(nftStaking.address);
+  const deployer = await getDeployer();
+  const configFileHelper = new ConfigFileHelper();
+  const config = configFileHelper.getConfig();
+
+  console.log(">> Start Add Pool for NFTStaking");
+  if (!config.NFT?.AlpiesStaking) throw Error("NFT contract address not found");
+  const NFTStaking = NFTStaking__factory.connect(config.NFT.AlpiesStaking, deployer);
+
+  for (const input of nftContractConfigsInput) {
+    console.log(`>> Adding Pool NFTStaking contract [${input.address}]`);
+    await NFTStaking.addPool(input.address, input.poolWeigth, input.minPeriod, input.maxPeriod);
+    console.log("✅ Done");
+  }
 };
 
 export default func;
-func.tags = ["NFTStaking"];
+func.tags = ["NFTStakingAddPool"];
