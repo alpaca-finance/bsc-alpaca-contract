ca4d38a449de005be63d0f4274c0f48f1a7145aa
sora-alpaca <sora.alpaca@gmail.com>
2022-06-10 11:30:02 +0700

    chore: add comment & remove unused code

--- solidity/contracts/8.13/nft-staking/NFTStaking.sol
+++ solidity/contracts/8.13/nft-staking/NFTStaking.sol
@@ -144,11 +144,12 @@ contract NFTStaking is INFTStaking, OwnableUpgradeable, ReentrancyGuardUpgradeab
     uint256 _lockUntil
   ) external nonReentrant onlyEOA {
     // Check
+    // reset _lockUntil to be current block timestamp
+    // in case caller want to send current timestamp but less than timestamp with block timestamp.
     _lockUntil = _lockUntil < block.timestamp ? block.timestamp : _lockUntil;
     if (poolInfo[_nftAddress].poolWeight == 0) revert NFTStaking_PoolNotExist();
     bytes32 _depositId = keccak256(abi.encodePacked(_nftAddress, msg.sender, _nftTokenId));
     if (userStakingNFTLockUntil[_depositId] != 0) revert NFTStaking_NFTAlreadyStaked();
-    if (_lockUntil < block.timestamp) revert NFTStaking_InvalidLockPeriod();
     uint256 _lockPeriod = _lockUntil - block.timestamp;
     if (_lockPeriod < poolInfo[_nftAddress].minLockPeriod || _lockPeriod > poolInfo[_nftAddress].maxLockPeriod)
       revert NFTStaking_InvalidLockPeriod();
