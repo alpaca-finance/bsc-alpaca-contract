f226472b418c6b66737398be2c7457eed7428e0c
black.alpaca <black.alpacaa@gmail.com>
2022-06-15 17:21:33 +0700

    feat: add set-swap-fee deployment script at delta config.

--- deploy/exec/delta-neutral-vault-config/config/set-swap-fee.ts
+++ deploy/exec/delta-neutral-vault-config/config/set-swap-fee.ts
@@ -17,52 +17,27 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     Check all variables below before execute the deployment script
     */
 
+  const swapFee = 25;
+  const swapFeeDenom = 10000;
   const DELTA_VAULT_SYMBOL = ["n8x-BNBUSDT-PCS2", "n8x-BNBUSDT-PCS1", "n3x-BNBBUSD-PCS1"];
 
   const deployer = await getDeployer();
-  const config = getConfig();
 
   // VALIDATING ALL DELTA_VAULT_SYMBOL
   const converter = new Converter();
   const configs = converter.convertDeltaSymbolToAddress(DELTA_VAULT_SYMBOL, "config");
-  const stableWorkers = converter.convertDeltaSymbolToAddress(DELTA_VAULT_SYMBOL, "stableDeltaWorker");
-  const assetWorkers = converter.convertDeltaSymbolToAddress(DELTA_VAULT_SYMBOL, "assetDeltaWorker");
-  const stableVaults = converter.convertDeltaSymbolToAddress(DELTA_VAULT_SYMBOL, "stableVault");
-  const assetVaults = converter.convertDeltaSymbolToAddress(DELTA_VAULT_SYMBOL, "assetVault");
 
-  console.log(">> Set Strategies to DeltaNeutralVaultConfig03 contract");
+  console.log(">> Set SwapFee to DeltaNeutralVaultConfig03 contract");
   let nonce = await deployer.getTransactionCount();
   const ops = isFork() ? { nonce: nonce++, gasLimit: 2000000 } : { nonce: nonce++ };
-  for (let i: number = 0; i < configs.length; i++) {
-    const stableVault = config.Vaults.find((v) => compare(v.address, stableVaults[i]));
-    const assetVault = config.Vaults.find((v) => compare(v.address, assetVaults[i]));
-    const stableWorker = stableVault?.workers.find((w) => compare(w.address, stableWorkers[i]));
-    const assetWorker = assetVault?.workers.find((w) => compare(w.address, assetWorkers[i]));
-    const deltaVaultConfig = DeltaNeutralVaultConfig03__factory.connect(configs[i], deployer);
-    console.log(
-      `>> Set StrategyPartialCloseMinimizeTrading: ${
-        stableWorker!.strategies.StrategyPartialCloseMinimizeTrading
-      } for config: ${configs[i]}`
-    );
-    console.log(
-      `>> Set StableStrategyAddTwoSidesOptimal: ${stableWorker!.strategies.StrategyAddTwoSidesOptimal} for config: ${
-        configs[i]
-      }`
-    );
-    console.log(
-      `>> Set AssetStrategyPartialCloseMinimizeTrading: ${
-        assetWorker!.strategies.StrategyAddTwoSidesOptimal
-      } for config: ${configs[i]}`
-    );
-    await deltaVaultConfig.setStrategies(
-      stableWorker!.strategies.StrategyPartialCloseMinimizeTrading,
-      stableWorker!.strategies.StrategyAddTwoSidesOptimal,
-      assetWorker!.strategies.StrategyAddTwoSidesOptimal,
-      ops
-    );
+  for (const config of configs) {
+    console.log(`>> Set SwapFee to ${swapFee} SwapFeeDenom to ${swapFeeDenom} for config : ${config}`);
+    const deltaVaultConfig = DeltaNeutralVaultConfig03__factory.connect(config, deployer);
+
+    await deltaVaultConfig.setSwapConfig(swapFee, swapFeeDenom, ops);
     console.log("✅ Done");
   }
 };
 
 export default func;
-func.tags = ["DeltaNeutralVaultConfigSetStrategies"];
+func.tags = ["DeltaNeutralVaultConfigSetSwapFee"];
