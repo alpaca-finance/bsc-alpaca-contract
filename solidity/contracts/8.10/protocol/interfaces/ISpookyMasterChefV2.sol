018360cccfff61fd651e09367a3c19f4fed7a418
spicysquid168 <spicysquid168@gmail.com>
2022-07-13 19:01:49 +0700

    feat: delta worker migration

--- solidity/contracts/8.10/protocol/interfaces/ISpookyMasterChefV2.sol
+++ solidity/contracts/8.10/protocol/interfaces/ISpookyMasterChefV2.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "./IERC20.sol";
+import "./ISpookyRewarder.sol";
 
 interface ISpookyMasterChefV2 {
   struct UserInfo {
@@ -8,18 +10,32 @@ interface ISpookyMasterChefV2 {
     uint256 rewardDebt; // Reward debt. See explanation below.
   }
 
+  // Info of each pool.
   struct PoolInfo {
-    IERC20 lpToken; // Address of LP token contract.
-    uint256 allocPoint; // How many allocation points assigned to this pool. BOO to distribute per second.
-    uint256 lastRewardBlock; // Last block number that SUSHI distribution occurs.
-    uint256 accBooPerShare; // Accumulated BOO per share, times 1e12. See below.
+    uint128 accBooPerShare;
+    uint64 lastRewardTime;
+    uint64 allocPoint;
   }
 
-  function poolInfo(uint256 pid) external view returns (ISpookyMasterChefV2.PoolInfo memory);
+  function MASTER_CHEF() external returns (address);
+
+  function BOO() external returns (IERC20);
+
+  function userInfo(uint256 pid, address user) external view returns (UserInfo memory);
+
+  function poolInfo(uint256 pid) external view returns (PoolInfo memory);
+
+  function rewarder(uint256 pid) external view returns (ISpookyRewarder);
+
+  function lpToken(uint256 index) external view returns (IERC20);
 
   function totalAllocPoint() external view returns (uint256);
 
   function booPerSecond() external view returns (uint256);
 
   function deposit(uint256 _pid, uint256 _amount) external;
+
+  function withdraw(uint256 _pid, uint256 _amount) external;
+
+  function harvestFromMasterChef() external;
 }
