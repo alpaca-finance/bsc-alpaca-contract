4537cf08dee995ce4c9d9dafc9c52b46790cc6a4
abc-alpaca <gabcdef637@gmail.com>
2022-07-19 14:33:42 +0700

    Revert "chore: remove .wait"
    This reverts commit f011e38795d69728f8420f412030a24749b8d743.

--- deploy/exec/delta-neutral-vault/config/set-healthchecker.ts
+++ deploy/exec/delta-neutral-vault/config/set-healthchecker.ts
@@ -50,7 +50,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     console.log(`> Setting Config ${vault.symbol} setDeltaNeutralVaultHealthChecker`);
     const deltaNeutralVault04 = DeltaNeutralVault04__factory.connect(vault.address, deployer);
     const ops = isFork() ? { nonce: nonce++, gasLimit: 2000000 } : { nonce: nonce++ };
-    await deltaNeutralVault04.setDeltaNeutralVaultHealthChecker(config.DeltaNeutralVaultHealthChecker, ops);
+    const tx = await deltaNeutralVault04.setDeltaNeutralVaultHealthChecker(config.DeltaNeutralVaultHealthChecker, ops);
+    await tx.wait(3);
     const checker = await deltaNeutralVault04.checker();
     if (compare(checker, config.DeltaNeutralVaultHealthChecker)) {
       console.log(`✅ Checking Done  ${vault.symbol}`);
